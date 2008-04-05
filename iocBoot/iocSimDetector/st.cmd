2e740af82a7170dfa2329257a9c77486c6a6acb4
rivers <rivers@dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b>
2008-04-05 03:26:01 +0000

    Added second sim detector; new syntax for Config commands
    git-svn-id: https://subversion.xor.aps.anl.gov/synApps/areaDetector/trunk@6985 dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b

--- iocBoot/iocSimDetector/st.cmd
+++ iocBoot/iocSimDetector/st.cmd
@@ -3,14 +3,22 @@ errlogInit(20000)
 
 dbLoadDatabase("$(AD)/dbd/simDetectorApp.dbd")
 simDetectorApp_registerRecordDeviceDriver(pdbbase) 
-dbLoadRecords("$(AD)/ADApp/Db/ADBase.template",     "P=13SIM1:,D=cam1:,PORT=SIM1,ADDR=0,TIMEOUT=1")
-dbLoadRecords("$(AD)/ADApp/Db/simDetector.template","P=13SIM1:,D=cam1:,PORT=SIM1,ADDR=0,TIMEOUT=1")
-dbLoadRecords("$(AD)/ADApp/Db/ADImage.template","P=13SIM1:,D=cam1:,PORT=SIM1Image,ADDR=0,TIMEOUT=1,SIZE=8,FTVL=UCHAR,NPIXELS=1392640")
 
 simDetectorConfig("SIM1", 640, 480, 1)
-drvADImageConfigure("SIM1Image", "SIM1")
+drvADImageConfigure("SIM1Image", 3, "SIM1", 0)
+dbLoadRecords("$(AD)/ADApp/Db/ADBase.template",     "P=13SIM1:,D=cam1:,PORT=SIM1,ADDR=0,TIMEOUT=1")
+dbLoadRecords("$(AD)/ADApp/Db/simDetector.template","P=13SIM1:,D=cam1:,PORT=SIM1,ADDR=0,TIMEOUT=1")
+
+dbLoadRecords("$(AD)/ADApp/Db/ADImage.template","P=13SIM1:,I=image1:,PORT=SIM1Image,ADDR=0,TIMEOUT=1,SIZE=8,FTVL=UCHAR,NPIXELS=1392640")
+
+simDetectorConfig("SIM2", 300, 200, 1)
+drvADImageConfigure("SIM2Image", 1, "SIM2", 0)
+dbLoadRecords("$(AD)/ADApp/Db/ADBase.template",     "P=13SIM1:,D=cam2:,PORT=SIM2,ADDR=0,TIMEOUT=1")
+dbLoadRecords("$(AD)/ADApp/Db/simDetector.template","P=13SIM1:,D=cam2:,PORT=SIM2,ADDR=0,TIMEOUT=1")
+dbLoadRecords("$(AD)/ADApp/Db/ADImage.template","P=13SIM1:,I=image2:,PORT=SIM2Image,ADDR=0,TIMEOUT=1,SIZE=8,FTVL=UCHAR,NPIXELS=1392640")
 
 #asynSetTraceMask("SIM1",0,255)
+#asynSetTraceMask("SIM2",0,255)
 
 set_requestfile_path("./")
 set_savefile_path("./autosave")
@@ -23,4 +31,4 @@ dbLoadRecords("$(AUTOSAVE)/asApp/Db/save_restoreStatus.db", "P=13SIM1:")
 iocInit()
 
 # save things every thirty seconds
-create_monitor_set("auto_settings.req", 30, "P=13SIM1:, D=cam1:")
+create_monitor_set("auto_settings.req", 30, "P=13SIM1:")
