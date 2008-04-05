d1aea4a12e585e7b282ae9d3424453cdfa60dc20
rivers <rivers@dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b>
2008-04-05 03:11:15 +0000

    Removed parameter string definitions, put back in C code
    git-svn-id: https://subversion.xor.aps.anl.gov/synApps/areaDetector/trunk@6972 dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b

--- ADApp/simDetectorSrc/drvSimDetector.h
+++ ADApp/simDetectorSrc/drvSimDetector.h
@@ -16,31 +16,6 @@
 extern "C" {
 #endif
 
-/* Note that the file format enum must agree with the mbbo/mbbi records in the simDetector.template file */
-typedef enum {
-   SimFormatBinary,
-   SimFormatASCII
-} SimFormat_t;
-
-/* If we have any private driver parameters they begin with ADFirstDriverParam and should end
-   with ADLastDriverParam, which is used for setting the size of the parameter library table */
-typedef enum {
-   SimGainX = ADFirstDriverParam,
-   SimGainY,
-   SimResetImage,
-   ADLastDriverParam
-} SimDetParam_t;
-
-/* The command strings are the input to ADUtils->FindParam, which returns the corresponding parameter enum value */
-static ADParamString_t SimDetParamString[] = {
-    {SimGainX,      "SIM_GAINX"},  
-    {SimGainY,      "SIM_GAINY"},  
-    {SimResetImage, "RESET_IMAGE"}  
-};
-
-#define NUM_SIM_DET_PARAMS (sizeof(SimDetParamString)/sizeof(SimDetParamString[0]))
-
-
 int simDetectorConfig(const char *portName, int maxSizeX, int maxSizeY, int dataType);
 
 #ifdef __cplusplus
