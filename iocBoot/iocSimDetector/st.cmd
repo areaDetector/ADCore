3e767f3efd6d6a008b98d9dd808142918c2b442b
rivers <rivers@dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b>
2008-04-08 20:32:12 +0000

    Start up plugins
    git-svn-id: https://subversion.xor.aps.anl.gov/synApps/areaDetector/trunk@7013 dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b

--- iocBoot/iocSimDetector/st.cmd
+++ iocBoot/iocSimDetector/st.cmd
@@ -4,18 +4,30 @@ errlogInit(20000)
 dbLoadDatabase("$(AD)/dbd/simDetectorApp.dbd")
 simDetectorApp_registerRecordDeviceDriver(pdbbase) 
 
+# Initialize the ADImageBuff buffer allocation library
+ADImageBuffInit(50, 50000000)
+
+# Create a simDetector driver
 simDetectorConfig("SIM1", 640, 480, 1)
-drvADImageConfigure("SIM1Image", 3, "SIM1", 0)
 dbLoadRecords("$(AD)/ADApp/Db/ADBase.template",     "P=13SIM1:,D=cam1:,PORT=SIM1,ADDR=0,TIMEOUT=1")
 dbLoadRecords("$(AD)/ADApp/Db/simDetector.template","P=13SIM1:,D=cam1:,PORT=SIM1,ADDR=0,TIMEOUT=1")
 
-dbLoadRecords("$(AD)/ADApp/Db/ADImage.template","P=13SIM1:,I=image1:,PORT=SIM1Image,ADDR=0,TIMEOUT=1,SIZE=8,FTVL=UCHAR,NPIXELS=1392640")
-
+# Create a second simDetector driver
 simDetectorConfig("SIM2", 300, 200, 1)
-drvADImageConfigure("SIM2Image", 1, "SIM2", 0)
 dbLoadRecords("$(AD)/ADApp/Db/ADBase.template",     "P=13SIM1:,D=cam2:,PORT=SIM2,ADDR=0,TIMEOUT=1")
 dbLoadRecords("$(AD)/ADApp/Db/simDetector.template","P=13SIM1:,D=cam2:,PORT=SIM2,ADDR=0,TIMEOUT=1")
+
+# Create an image plugin, set it to get data from first simDetector driver.
+drvADImageConfigure("SIM1Image", 3, 0, "SIM1", 0)
+dbLoadRecords("$(AD)/ADApp/Db/ADImage.template","P=13SIM1:,I=image1:,PORT=SIM1Image,ADDR=0,TIMEOUT=1,SIZE=8,FTVL=UCHAR,NPIXELS=1392640")
+
+# Create a second image plugin, set it to get data from second simDetector driver.
+drvADImageConfigure("SIM2Image", 1, 0, "SIM2", 0)
 dbLoadRecords("$(AD)/ADApp/Db/ADImage.template","P=13SIM1:,I=image2:,PORT=SIM2Image,ADDR=0,TIMEOUT=1,SIZE=8,FTVL=UCHAR,NPIXELS=1392640")
+
+# Create a file saving plugin
+drvADFileConfigure("SIM1File", 20, 0, "SIM1", 0)
+dbLoadRecords("$(AD)/ADApp/Db/ADFile.template","P=13SIM1:,F=file1:,PORT=SIM1File,ADDR=0,TIMEOUT=1")
 
 #asynSetTraceMask("SIM1",0,255)
 #asynSetTraceMask("SIM2",0,255)
