731711fc1e443a27dee39bb80dfd3c50ef70b232
rivers <rivers@dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b>
2008-04-18 19:44:18 +0000

    New file for netCDF support
    git-svn-id: https://subversion.xor.aps.anl.gov/synApps/areaDetector/trunk@7073 dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b

---
+++ ADApp/pluginSrc/NDFileNetCDF.h
@@ -0,0 +1,33 @@
+/*
+ * NDFileNetCDF.h
+ * Writes NDArrays to netCDF files.
+ *
+ * Mark Rivers
+ * April 17, 2008
+ */
+
+#include "NDArrayBuff.h"
+
+#ifndef DRV_NDFileNetCDF_H
+#define DRV_NDFileNetCDF_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct NDFileNetCDFState {
+    int ncId;
+    int arrayDataId;
+    int uniqueIdId;
+    int timeStampId;
+    int nextRecord;
+} NDFileNetCDFState_t;
+
+
+int NDFileWriteNetCDF(const char *fileName, NDFileNetCDFState_t *pState, NDArray_t *pArray, 
+                      int numArrays, int append, int close);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
