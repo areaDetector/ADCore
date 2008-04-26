54e864694bea2c155cbab5180f056b43353326d4
rivers <rivers@dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b>
2008-04-26 13:29:02 +0000

    Load NDpluginBase.template for each plugin; change macro parameters to R for everything
    git-svn-id: https://subversion.xor.aps.anl.gov/synApps/areaDetector/trunk@7147 dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b

--- iocBoot/iocSimDetector/st.cmd
+++ iocBoot/iocSimDetector/st.cmd
@@ -10,25 +10,28 @@ NDArrayBuffInit(50, 50000000)
 
 # Create a simDetector driver
 simDetectorConfig("SIM1", 640, 480, 1)
-dbLoadRecords("$(AD)/ADApp/Db/ADBase.template",     "P=13SIM1:,D=cam1:,PORT=SIM1,ADDR=0,TIMEOUT=1")
-dbLoadRecords("$(AD)/ADApp/Db/simDetector.template","P=13SIM1:,D=cam1:,PORT=SIM1,ADDR=0,TIMEOUT=1")
+dbLoadRecords("$(AD)/ADApp/Db/ADBase.template",     "P=13SIM1:,R=cam1:,PORT=SIM1,ADDR=0,TIMEOUT=1")
+dbLoadRecords("$(AD)/ADApp/Db/simDetector.template","P=13SIM1:,R=cam1:,PORT=SIM1,ADDR=0,TIMEOUT=1")
 
 # Create a second simDetector driver
 simDetectorConfig("SIM2", 300, 200, 1)
-dbLoadRecords("$(AD)/ADApp/Db/ADBase.template",     "P=13SIM1:,D=cam2:,PORT=SIM2,ADDR=0,TIMEOUT=1")
-dbLoadRecords("$(AD)/ADApp/Db/simDetector.template","P=13SIM1:,D=cam2:,PORT=SIM2,ADDR=0,TIMEOUT=1")
+dbLoadRecords("$(AD)/ADApp/Db/ADBase.template",     "P=13SIM1:,R=cam2:,PORT=SIM2,ADDR=0,TIMEOUT=1")
+dbLoadRecords("$(AD)/ADApp/Db/simDetector.template","P=13SIM1:,R=cam2:,PORT=SIM2,ADDR=0,TIMEOUT=1")
 
 # Create a standard arrays plugin, set it to get data from first simDetector driver.
 drvNDStdArraysConfigure("SIM1Image", 3, 0, "SIM1", 0)
-dbLoadRecords("$(AD)/ADApp/Db/NDStdArrays.template","P=13SIM1:,A=image1:,PORT=SIM1Image,ADDR=0,TIMEOUT=1,SIZE=8,FTVL=UCHAR,NELEMENTS=1392640")
+dbLoadRecords("$(AD)/ADApp/Db/NDPluginBase.template","P=13SIM1:,R=image1:,PORT=SIM1Image,ADDR=0,TIMEOUT=1")
+dbLoadRecords("$(AD)/ADApp/Db/NDStdArrays.template","P=13SIM1:,R=image1:,PORT=SIM1Image,ADDR=0,TIMEOUT=1,SIZE=8,FTVL=UCHAR,NELEMENTS=1392640")
 
 # Create a standard arrays plugin, set it to get data from second simDetector driver.
 drvNDStdArraysConfigure("SIM2Image", 1, 0, "SIM2", 0)
-dbLoadRecords("$(AD)/ADApp/Db/NDStdArrays.template","P=13SIM1:,A=image2:,PORT=SIM2Image,ADDR=0,TIMEOUT=1,SIZE=8,FTVL=UCHAR,NELEMENTS=1392640")
+dbLoadRecords("$(AD)/ADApp/Db/NDPluginBase.template","P=13SIM1:,R=image2:,PORT=SIM2Image,ADDR=0,TIMEOUT=1")
+dbLoadRecords("$(AD)/ADApp/Db/NDStdArrays.template","P=13SIM1:,R=image2:,PORT=SIM2Image,ADDR=0,TIMEOUT=1,SIZE=8,FTVL=UCHAR,NELEMENTS=1392640")
 
 # Create a file saving plugin
 drvNDFileConfigure("SIM1File", 20, 0, "SIM1", 0)
-dbLoadRecords("$(AD)/ADApp/Db/NDFile.template","P=13SIM1:,F=file1:,PORT=SIM1File,ADDR=0,TIMEOUT=1")
+dbLoadRecords("$(AD)/ADApp/Db/NDPluginBase.template","P=13SIM1:,R=file1:,PORT=SIM1File,ADDR=0,TIMEOUT=1")
+dbLoadRecords("$(AD)/ADApp/Db/NDFile.template","P=13SIM1:,R=file1:,PORT=SIM1File,ADDR=0,TIMEOUT=1")
 
 # Create an ROI plugin
 drvNDROIConfigure("SIM1ROI", 20, 0, "SIM1", 0, 4)
