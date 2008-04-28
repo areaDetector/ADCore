013963c0590dfa954c582d27107b4af4597069ae
rivers <rivers@dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b>
2008-04-28 23:16:11 +0000

    New base class for asyn and paramLib
    git-svn-id: https://subversion.xor.aps.anl.gov/synApps/areaDetector/trunk@7150 dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b

---
+++ ADApp/pluginSrc/asynParamBase.h
@@ -0,0 +1,79 @@
+#ifndef asynParamBase_H
+#define asynParamBase_H
+
+#include <epicsTypes.h>
+#include <asynStandardInterfaces.h>
+
+#include "ADParamLib.h"
+#include "NDArrayBuff.h"
+
+class asynParamBase {
+public:
+    asynParamBase(const char *portName, int maxAddr, int paramTableSize);
+    virtual asynStatus getAddress(asynUser *pasynUser, const char *functionName, int *address); 
+    virtual asynStatus readInt32(asynUser *pasynUser, epicsInt32 *value);
+    virtual asynStatus writeInt32(asynUser *pasynUser, epicsInt32 value);
+    virtual asynStatus getBounds(asynUser *pasynUser, epicsInt32 *low, epicsInt32 *high);
+    virtual asynStatus readFloat64(asynUser *pasynUser, epicsFloat64 *value);
+    virtual asynStatus writeFloat64(asynUser *pasynUser, epicsFloat64 value);
+    virtual asynStatus readOctet(asynUser *pasynUser, char *value, size_t maxChars,
+                         size_t *nActual, int *eomReason);
+    virtual asynStatus writeOctet(asynUser *pasynUser, const char *value, size_t maxChars,
+                          size_t *nActual);
+    virtual asynStatus readInt8Array(asynUser *pasynUser, epicsInt8 *value, 
+                                        size_t nElements, size_t *nIn);
+    virtual asynStatus writeInt8Array(asynUser *pasynUser, epicsInt8 *value,
+                                        size_t nElements);
+    virtual asynStatus doCallbacksInt8Array(epicsInt8 *value,
+                                        size_t nElements, int reason, int addr);
+    virtual asynStatus readInt16Array(asynUser *pasynUser, epicsInt16 *value,
+                                        size_t nElements, size_t *nIn);
+    virtual asynStatus writeInt16Array(asynUser *pasynUser, epicsInt16 *value,
+                                        size_t nElements);
+    virtual asynStatus doCallbacksInt16Array(epicsInt16 *value,
+                                        size_t nElements, int reason, int addr);
+    virtual asynStatus readInt32Array(asynUser *pasynUser, epicsInt32 *value,
+                                        size_t nElements, size_t *nIn);
+    virtual asynStatus writeInt32Array(asynUser *pasynUser, epicsInt32 *value,
+                                        size_t nElements);
+    virtual asynStatus doCallbacksInt32Array(epicsInt32 *value,
+                                        size_t nElements, int reason, int addr);
+    virtual asynStatus readFloat32Array(asynUser *pasynUser, epicsFloat32 *value,
+                                        size_t nElements, size_t *nIn);
+    virtual asynStatus writeFloat32Array(asynUser *pasynUser, epicsFloat32 *value,
+                                        size_t nElements);
+    virtual asynStatus doCallbacksFloat32Array(epicsFloat32 *value,
+                                        size_t nElements, int reason, int addr);
+    virtual asynStatus readFloat64Array(asynUser *pasynUser, epicsFloat64 *value,
+                                        size_t nElements, size_t *nIn);
+    virtual asynStatus writeFloat64Array(asynUser *pasynUser, epicsFloat64 *value,
+                                        size_t nElements);
+    virtual asynStatus doCallbacksFloat64Array(epicsFloat64 *value,
+                                        size_t nElements, int reason, int addr);
+    virtual asynStatus readNDArray(asynUser *pasynUser, void *handle);
+    virtual asynStatus writeNDArray(asynUser *pasynUser, void *handle);
+    virtual asynStatus doCallbacksNDArray(void *handle, int reason, int addr);
+    virtual asynStatus drvUserCreate(asynUser *pasynUser, const char *drvInfo, 
+                                     const char **pptypeName, size_t *psize);
+    virtual asynStatus drvUserGetType(asynUser *pasynUser,
+                                        const char **pptypeName, size_t *psize);
+    virtual asynStatus drvUserDestroy(asynUser *pasynUser);
+    virtual void report(FILE *fp, int details);
+    virtual asynStatus connect(asynUser *pasynUser);
+    virtual asynStatus disconnect(asynUser *pasynUser);
+   
+    char *portName;
+    int maxAddr;
+    PARAMS *params;
+    NDArray_t **pArrays;
+    epicsMutexId mutexId;
+
+    /* The asyn interfaces this driver implements */
+    asynStandardInterfaces asynStdInterfaces;
+    
+    /* asynUser connected to ourselves for asynTrace */
+    asynUser *pasynUser;
+};
+
+    
+#endif
