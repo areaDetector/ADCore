276262c17edfe51522b591c36ab95777dc1bed5a
rivers <rivers@dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b>
2008-04-28 23:35:54 +0000

    Removed unneeded methods; added template functions
    git-svn-id: https://subversion.xor.aps.anl.gov/synApps/areaDetector/trunk@7155 dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b

--- ADApp/pluginSrc/NDPluginStdArrays.h
+++ ADApp/pluginSrc/NDPluginStdArrays.h
@@ -9,10 +9,8 @@
 
 typedef enum
 {
-    NDPluginStdArraysNDimensions    /* (asynInt32,    r/o) Number of dimensions in array */
+    NDPluginStdArraysData           /* (asynXXXArray, r/w) Array data waveform */
         = NDPluginBaseLastParam,
-    NDPluginStdArraysDimensions,    /* (asynInt32Array, r/o) Array dimensions */
-    NDPluginStdArraysData,          /* (asynXXXArray, r/w) Array data waveform */
     NDPluginStdArraysLastParam
 } NDPluginStdArraysParam_t;
 
@@ -20,9 +18,7 @@ typedef enum
  * The asynDrvUser interface in this driver parses these strings and puts the
  * corresponding enum value in pasynUser->reason */
 static ADParamString_t NDPluginStdArraysParamString[] = {
-    {NDPluginStdArraysNDimensions,        "ARRAY_NDIMENSIONS"},
-    {NDPluginStdArraysDimensions,         "ARRAY_DIMENSIONS"},
-    {NDPluginStdArraysData,               "ARRAY_DATA"}
+    {NDPluginStdArraysData,               "STD_ARRAY_DATA"}
 };
 
 #define NUM_ND_PLUGIN_STD_ARRAYS_PARAMS (sizeof(NDPluginStdArraysParamString)/sizeof(NDPluginStdArraysParamString[0]))
@@ -31,24 +27,25 @@ class NDPluginStdArrays : public NDPluginBase {
 public:
     NDPluginStdArrays(const char *portName, int queueSize, int blockingCallbacks, 
                       const char *NDArrayPort, int NDArrayAddr);
+
     /* These methods override the virtual methods in the base class */
     void processCallbacks(NDArray_t *pArray);
-    asynStatus writeInt32(asynUser *pasynUser, epicsInt32 value);
-    asynStatus writeFloat64(asynUser *pasynUser, epicsFloat64 value);
-    asynStatus writeOctet(asynUser *pasynUser, const char *value, size_t maxChars,
-                          size_t *nActual);
+    virtual asynStatus readInt8Array(asynUser *pasynUser, epicsInt8 *value,
+                                        size_t nElements, size_t *nIn);
+    virtual asynStatus readInt16Array(asynUser *pasynUser, epicsInt16 *value,
+                                        size_t nElements, size_t *nIn);
+    virtual asynStatus readInt32Array(asynUser *pasynUser, epicsInt32 *value,
+                                        size_t nElements, size_t *nIn);
+    virtual asynStatus readFloat32Array(asynUser *pasynUser, epicsFloat32 *value,
+                                        size_t nElements, size_t *nIn);
+    virtual asynStatus readFloat64Array(asynUser *pasynUser, epicsFloat64 *value,
+                                        size_t nElements, size_t *nIn);
     asynStatus drvUserCreate(asynUser *pasynUser, const char *drvInfo, 
-                             const char **pptypeName, size_t *psize);
-                             
-    /* These methods are unique to this class */
-    asynStatus readInt8Array(asynUser *pasynUser, epicsInt8 *value, size_t nElements, size_t *nIn);
-    asynStatus readInt16Array(asynUser *pasynUser, epicsInt16 *value, size_t nElements, size_t *nIn);
-    asynStatus readInt32Array(asynUser *pasynUser,    epicsInt32 *value, size_t nElements, size_t *nIn);
-    asynStatus readFloat32Array(asynUser *pasynUser, epicsFloat32 *value, size_t nElements, size_t *nIn);
-    asynStatus readFloat64Array(asynUser *pasynUser, epicsFloat64 *value, size_t nElements, size_t *nIn);
+                                        const char **pptypeName, size_t *psize);
 
-private:
-    int dimsPrev[ND_ARRAY_MAX_DIMS];
+    /* These methods are just for this class */
+    template <typename epicsType> asynStatus readArray(asynUser *pasynUser, epicsType *value, 
+                                        size_t nElements, size_t *nIn, int outputType);                            
 };
 
     
