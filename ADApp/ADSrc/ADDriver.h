418ad3421fc2d4cbb34ad9ea92c101d1b6689509
rivers <rivers@dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b>
2008-11-11 21:27:11 +0000

    Added setShutter() and writeInt32() methods
    git-svn-id: https://subversion.xor.aps.anl.gov/synApps/areaDetector/trunk@7777 dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b

--- ADApp/ADSrc/ADDriver.h
+++ ADApp/ADSrc/ADDriver.h
@@ -14,11 +14,13 @@ public:
              int interfaceMask, int interruptMask);
                  
     /* These are the methods that we override from asynPortDriver */
-     virtual asynStatus drvUserCreate(asynUser *pasynUser, const char *drvInfo, 
+    virtual asynStatus drvUserCreate(asynUser *pasynUser, const char *drvInfo, 
                                      const char **pptypeName, size_t *psize);
                                      
     /* These are the methods that are new to this class */
+    virtual asynStatus writeInt32(asynUser *pasynUser, epicsInt32 value);
     int createFileName(int maxChars, char *fullFileName);
+    void setShutter(int open);
 
     /* Our data */
 };
