f38ac09f1870068b9ecfc2f8c0d3b7b2dae78831
rivers <rivers@dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b>
2009-05-01 21:23:21 +0000

    Moved createFileName here from ADDriver.cpp; started adding support for PVAttributes
    git-svn-id: https://subversion.xor.aps.anl.gov/synApps/areaDetector/trunk@8863 dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b

--- ADApp/ADSrc/asynNDArrayDriver.h
+++ ADApp/ADSrc/asynNDArrayDriver.h
@@ -3,6 +3,8 @@
 
 #include "asynPortDriver.h"
 #include "NDArray.h"
+#include "PVAttributes.h"
+
 /** This is the class from which NDArray drivers are derived; implements the asynGenericPointer functions, assuming
   * that these reference NDArray objects. 
   * For areaDetector, both plugins and detector drivers are indirectly derived from this class.
@@ -13,13 +15,24 @@ public:
     asynNDArrayDriver(const char *portName, int maxAddr, int paramTableSize, int maxBuffers, size_t maxMemory,
                       int interfaceMask, int interruptMask,
                       int asynFlags, int autoConnect, int priority, int stackSize);
+
+    /* These are the methods that we override from asynPortDriver */
     virtual asynStatus readGenericPointer(asynUser *pasynUser, void *genericPointer);
     virtual asynStatus writeGenericPointer(asynUser *pasynUser, void *genericPointer);
+    virtual asynStatus drvUserCreate(asynUser *pasynUser, const char *drvInfo,
+                                     const char **pptypeName, size_t *psize);
     virtual void report(FILE *fp, int details);
+
+    /* These are the methods that are new to this class */
+    virtual int createFileName(int maxChars, char *fullFileName);
+    virtual int createFileName(int maxChars, char *filePath, char *fileName);
+    virtual int readPVAttributesFile(const char *fileName);
 
 protected:
     NDArray **pArrays;             /**< An array of NDArray pointers used to store data in the driver */
     NDArrayPool *pNDArrayPool;     /**< An NDArrayPool object used to allocate and manipulate NDArray objects */
+    PVAttributes *pPVAttributes;   /**< A PVAttributes object used to obtain the current values of a set of
+                                     *  EPICS PVs */
 };
 
 #endif
