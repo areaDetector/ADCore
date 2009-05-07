a9a9296948da3e7005286177f83b138ed63deea5
rivers <rivers@dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b>
2009-05-07 20:54:55 +0000

    Removed drv from configuration commands; create both netCDF and TIFF file plugins
    git-svn-id: https://subversion.xor.aps.anl.gov/synApps/areaDetector/trunk@8919 dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b

--- iocBoot/iocSimDetector/st.cmd
+++ iocBoot/iocSimDetector/st.cmd
@@ -16,24 +16,29 @@ dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/ADBase.template",     "P=13SIM1:,R=cam2
 dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/simDetector.template","P=13SIM1:,R=cam2:,PORT=SIM2,ADDR=0,TIMEOUT=1")
 
 # Create a standard arrays plugin, set it to get data from first simDetector driver.
-drvNDStdArraysConfigure("SIM1Image", 3, 0, "SIM1", 0, 2000000)
+NDStdArraysConfigure("SIM1Image", 3, 0, "SIM1", 0, 2000000)
 dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDPluginBase.template","P=13SIM1:,R=image1:,PORT=SIM1Image,ADDR=0,TIMEOUT=1,NDARRAY_PORT=SIM1,NDARRAY_ADDR=0")
 dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDStdArrays.template", "P=13SIM1:,R=image1:,PORT=SIM1Image,ADDR=0,TIMEOUT=1,SIZE=8,FTVL=UCHAR,NELEMENTS=1392640")
 
 # Create a standard arrays plugin, set it to get data from second simDetector driver.
-drvNDStdArraysConfigure("SIM2Image", 1, 0, "SIM2", 0, 2000000)
+NDStdArraysConfigure("SIM2Image", 1, 0, "SIM2", 0, 2000000)
 dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDPluginBase.template","P=13SIM1:,R=image2:,PORT=SIM2Image,ADDR=0,TIMEOUT=1,NDARRAY_PORT=SIM1,NDARRAY_ADDR=0")
 dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDStdArrays.template", "P=13SIM1:,R=image2:,PORT=SIM2Image,ADDR=0,TIMEOUT=1,SIZE=8,FTVL=UCHAR,NELEMENTS=1392640")
 # Load the database to use with Stephen Mudie's IDL code
 #dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/EPICS_AD_Viewer.template", "P=13SIM1:, R=image1:")
 
-# Create a file saving plugin
-drvNDFileNetCDFConfigure("SIM1File", 20, 0, "SIM1", 0)
-dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDPluginBase.template","P=13SIM1:,R=file1:,PORT=SIM1File,ADDR=0,TIMEOUT=1,NDARRAY_PORT=SIM1,NDARRAY_ADDR=0")
-dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDFile.template",      "P=13SIM1:,R=file1:,PORT=SIM1File,ADDR=0,TIMEOUT=1")
+# Create a netCDF file saving plugin
+NDFileNetCDFConfigure("SIM1FileNetCDF", 20, 0, "SIM1", 0)
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDPluginBase.template","P=13SIM1:,R=netCDF1:,PORT=SIM1FileNetCDF,ADDR=0,TIMEOUT=1,NDARRAY_PORT=SIM1,NDARRAY_ADDR=0")
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDFile.template",      "P=13SIM1:,R=netCDF1:,PORT=SIM1FileNetCDF,ADDR=0,TIMEOUT=1")
+
+# Create a TIFF file saving plugin
+NDFileTIFFConfigure("SIM1FileTIFF", 20, 0, "SIM1", 0)
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDPluginBase.template","P=13SIM1:,R=TIFF1:,PORT=SIM1FileTIFF,ADDR=0,TIMEOUT=1,NDARRAY_PORT=SIM1,NDARRAY_ADDR=0")
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDFile.template",      "P=13SIM1:,R=TIFF1:,PORT=SIM1FileTIFF,ADDR=0,TIMEOUT=1")
 
 # Create an ROI plugin
-drvNDROIConfigure("SIM1ROI", 20, 0, "SIM1", 0, 8, 20, 20000000)
+NDROIConfigure("SIM1ROI", 20, 0, "SIM1", 0, 8, 20, 20000000)
 dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDPluginBase.template","P=13SIM1:,R=ROI1:,  PORT=SIM1ROI,ADDR=0,TIMEOUT=1,NDARRAY_PORT=SIM1,NDARRAY_ADDR=0")
 dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDROI.template",       "P=13SIM1:,R=ROI1:,  PORT=SIM1ROI,ADDR=0,TIMEOUT=1")
 dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDROIN.template",      "P=13SIM1:,R=ROI1:0:,PORT=SIM1ROI,ADDR=0,TIMEOUT=1,HIST_SIZE=256")
