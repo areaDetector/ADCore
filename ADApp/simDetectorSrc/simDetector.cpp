875798294b1154546b761458ed11d6cfda3c2999
rivers <rivers@dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b>
2009-05-07 19:00:15 +0000

    Moved iocsh code into simDetector.cpp
    git-svn-id: https://subversion.xor.aps.anl.gov/synApps/areaDetector/trunk@8903 dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b

--- ADApp/simDetectorSrc/simDetector.cpp
+++ ADApp/simDetectorSrc/simDetector.cpp
@@ -25,15 +25,12 @@
 #include <epicsStdio.h>
 #include <epicsMutex.h>
 #include <cantProceed.h>
+#include <iocsh.h>
+#include <epicsExport.h>
 
-#include "ADStdDriverParams.h"
-#include "NDArray.h"
 #include "ADDriver.h"
 
-#include "drvSimDetector.h"
-
-
-static const char *driverName = "drvSimDetector";
+static const char *driverName = "simDetector";
 
 /** Simulation detector driver; demonstrates most of the features that areaDetector drivers can support. */
 class simDetector : public ADDriver {
@@ -689,14 +686,6 @@ void simDetector::report(FILE *fp, int details)
     ADDriver::report(fp, details);
 }
 
-extern "C" int simDetectorConfig(const char *portName, int maxSizeX, int maxSizeY, int dataType,
-                                 int maxBuffers, size_t maxMemory, int priority, int stackSize)
-{
-    new simDetector(portName, maxSizeX, maxSizeY, (NDDataType_t)dataType, 
-                    maxBuffers, maxMemory, priority, stackSize);
-    return(asynSuccess);
-}
-
 /** Constructor for simDetector; most parameters are simply passed to ADDriver::ADDriver.
   * After calling the base class constructor this method creates a thread to compute the simulated detector data, 
   * and sets reasonable default values for all of the parameters defined in this class and ADStdDriverParams.h.
@@ -777,3 +766,46 @@ simDetector::simDetector(const char *portName, int maxSizeX, int maxSizeY, NDDat
         return;
     }
 }
+
+/** Configuration command, called directly or from iocsh */
+extern "C" int simDetectorConfig(const char *portName, int maxSizeX, int maxSizeY, int dataType,
+                                 int maxBuffers, size_t maxMemory, int priority, int stackSize)
+{
+    new simDetector(portName, maxSizeX, maxSizeY, (NDDataType_t)dataType, 
+                    maxBuffers, maxMemory, priority, stackSize);
+    return(asynSuccess);
+}
+
+/** Code for iocsh registration */
+static const iocshArg simDetectorConfigArg0 = {"Port name", iocshArgString};
+static const iocshArg simDetectorConfigArg1 = {"Max X size", iocshArgInt};
+static const iocshArg simDetectorConfigArg2 = {"Max Y size", iocshArgInt};
+static const iocshArg simDetectorConfigArg3 = {"Data type", iocshArgInt};
+static const iocshArg simDetectorConfigArg4 = {"maxBuffers", iocshArgInt};
+static const iocshArg simDetectorConfigArg5 = {"maxMemory", iocshArgInt};
+static const iocshArg simDetectorConfigArg6 = {"priority", iocshArgInt};
+static const iocshArg simDetectorConfigArg7 = {"stackSize", iocshArgInt};
+static const iocshArg * const simDetectorConfigArgs[] =  {&simDetectorConfigArg0,
+                                                          &simDetectorConfigArg1,
+                                                          &simDetectorConfigArg2,
+                                                          &simDetectorConfigArg3,
+                                                          &simDetectorConfigArg4,
+                                                          &simDetectorConfigArg5,
+                                                          &simDetectorConfigArg6,
+                                                          &simDetectorConfigArg7};
+static const iocshFuncDef configsimDetector = {"simDetectorConfig", 8, simDetectorConfigArgs};
+static void configsimDetectorCallFunc(const iocshArgBuf *args)
+{
+    simDetectorConfig(args[0].sval, args[1].ival, args[2].ival, args[3].ival,
+                      args[4].ival, args[5].ival, args[6].ival, args[7].ival);
+}
+
+
+static void simDetectorRegister(void)
+{
+
+    iocshRegister(&configsimDetector, configsimDetectorCallFunc);
+}
+
+epicsExportRegistrar(simDetectorRegister);
+
