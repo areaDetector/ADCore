39f0057a278671ee6cfe4600b233352bb0a05945
rivers <rivers@dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b>
2009-05-07 18:28:16 +0000

    Moved iocsh commands into this file, deleted drvNDFileTIFF* files; removed tabs, fixed line terminators, other fixes
    git-svn-id: https://subversion.xor.aps.anl.gov/synApps/areaDetector/trunk@8886 dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b

--- ADApp/pluginSrc/NDFileTIFF.cpp
+++ ADApp/pluginSrc/NDFileTIFF.cpp
@@ -11,20 +11,14 @@
 #include <netcdf.h>
 
 #include <epicsStdio.h>
+#include <epicsExport.h>
+#include <iocsh.h>
 
 #include "NDPluginFile.h"
 #include "NDFileTIFF.h"
-#include "drvNDFileTIFF.h"
 #include "tiffio.h"
 
 static const char *driverName = "NDFileTIFF";
-
-/* Handle errors by printing an error message and exiting with a
- * non-zero status. */
-#define ERR(e) {asynPrint(this->pasynUserSelf, ASYN_TRACE_ERROR, \
-                "%s:%s error=%s\n", \
-                driverName, functionName, nc_strerror(e)); \
-                return(asynError);}
 
 /* NDArray string attributes can be of any length, but netCDF requires a fixed maximum length
  * which we define here. */
@@ -36,35 +30,30 @@ asynStatus NDFileTIFF::openFile(const char *fileName, NDFileOpenMode_t openMode,
 {
     /* When we create TIFF variables and dimensions, we get back an
      * ID for each one. */
-    nc_type ncType=NC_NAT;
     static const char *functionName = "openFile";
-	short bitsPerSample, samplesPerPixel;
+    short bitsPerSample=0, samplesPerPixel;
+
     /* We don't support reading yet */
     if (openMode & NDFileModeRead) return(asynError);
 
     /* We don't support opening an existing file for appending yet */
     if (openMode & NDFileModeAppend) return(asynError);
 
-    /* Set the next record in the file to 0 */
-    this->nextRecord = 0;
-	printf("FileName: %s\n", fileName);
-    /* Create the file. */
+   /* Create the file. */
     if ((this->output = TIFFOpen(fileName, "w")) == NULL ) {
-		asynPrint(this->pasynUserSelf, ASYN_TRACE_ERROR, "TIFF Plugin: Trouble Opening file %s\n",
-		fileName);
-
-        ERR(-1);
-	}
+        asynPrint(this->pasynUserSelf, ASYN_TRACE_ERROR, 
+        "%s:%s error opening file %s\n",
+        driverName, functionName, fileName);
+        return(asynError);
+    }
 
     switch (pArray->dataType) {
         case NDInt8:
         case NDUInt8:
-			printf ("Open 8 bit\n");
             bitsPerSample = 8;
             break;
         case NDInt16:
         case NDUInt16:
-			printf ("Open 16 bit\n");
             bitsPerSample = 16;
             break;
         case NDInt32:
@@ -79,12 +68,10 @@ asynStatus NDFileTIFF::openFile(const char *fileName, NDFileOpenMode_t openMode,
             break;
     }
     samplesPerPixel = 1;
-	TIFFSetField(this->output, TIFFTAG_BITSPERSAMPLE, bitsPerSample);
+    TIFFSetField(this->output, TIFFTAG_BITSPERSAMPLE, bitsPerSample);
     TIFFSetField(this->output, TIFFTAG_SAMPLESPERPIXEL, samplesPerPixel);
-	TIFFSetField (this->output, TIFFTAG_PHOTOMETRIC, PHOTOMETRIC_MINISBLACK);
-	TIFFSetField (this->output, TIFFTAG_PLANARCONFIG, PLANARCONFIG_CONTIG);
-
-
+    TIFFSetField (this->output, TIFFTAG_PHOTOMETRIC, PHOTOMETRIC_MINISBLACK);
+    TIFFSetField (this->output, TIFFTAG_PLANARCONFIG, PLANARCONFIG_CONTIG);
 
     return(asynSuccess);
 }
@@ -93,59 +80,62 @@ asynStatus NDFileTIFF::openFile(const char *fileName, NDFileOpenMode_t openMode,
  *  to add arrays to a single file in stream or capture mode */
 asynStatus NDFileTIFF::writeFile(NDArray *pArray)
 {
-	unsigned long int stripsize;
+    unsigned long int stripsize;
+    tsize_t nwrite=0;
     static const char *functionName = "writeFile";
-	int sizex, sizey;
-	char ManufacturerString[MAX_ATTRIBUTE_STRING_SIZE];
-	char ModelString[MAX_ATTRIBUTE_STRING_SIZE];
+    int sizex, sizey;
+    char ManufacturerString[MAX_ATTRIBUTE_STRING_SIZE] = "Unknown";
+    char ModelString[MAX_ATTRIBUTE_STRING_SIZE] = "Unknown";
 
-    getIntegerParam(ADImageSizeX, &sizex);
-	getIntegerParam(ADImageSizeY, &sizey);
-	getStringParam(ADManufacturer, MAX_ATTRIBUTE_STRING_SIZE, ManufacturerString);
-	getStringParam(ADModel, MAX_ATTRIBUTE_STRING_SIZE, ModelString);
+    getIntegerParam(NDArraySizeX, &sizex);
+    getIntegerParam(NDArraySizeY, &sizey);
+    /* We cannot get the manufacturer and model directly, we don't have access to the parameter
+     * library of the driver.  We would have to get this from attributes */
+    //getStringParam(ADManufacturer, MAX_ATTRIBUTE_STRING_SIZE, ManufacturerString);
+    //getStringParam(ADModel, MAX_ATTRIBUTE_STRING_SIZE, ModelString);
 
-    printf("Hello writeFile: %d, %d, %d, %d\n", sizex, sizey, pArray->dims[0].size, pArray->dims[1].size);
-	sizex = pArray->dims[0].size;
-	sizey = pArray->dims[1].size;
+    asynPrint(this->pasynUserSelf, ASYN_TRACE_FLOW,
+              "%s:s: %d, %d, %d, %d\n", 
+              driverName, functionName, sizex, sizey, pArray->dims[0].size, pArray->dims[1].size);
+    sizex = pArray->dims[0].size;
+    sizey = pArray->dims[1].size;
 
-	TIFFSetField(this->output, TIFFTAG_IMAGEWIDTH, sizex);
-	TIFFSetField(this->output, TIFFTAG_IMAGELENGTH, sizey);
-	TIFFSetField (this->output, TIFFTAG_ROWSPERSTRIP, sizey);  //save entire image in one strip.
-	TIFFSetField(this->output, TIFFTAG_MAKE, ManufacturerString);
-	TIFFSetField(this->output, TIFFTAG_MODEL, ModelString);
+    TIFFSetField(this->output, TIFFTAG_IMAGEWIDTH, sizex);
+    TIFFSetField(this->output, TIFFTAG_IMAGELENGTH, sizey);
+    TIFFSetField (this->output, TIFFTAG_ROWSPERSTRIP, sizey);  //save entire image in one strip.
+    TIFFSetField(this->output, TIFFTAG_MAKE, ManufacturerString);
+    TIFFSetField(this->output, TIFFTAG_MODEL, ModelString);
 
     stripsize = TIFFStripSize(this->output);
-
-
 
     switch (pArray->dataType) {
         case NDInt8:
         case NDUInt8:
-			if ( TIFFWriteEncodedStrip( this->output, 0, (signed char *)pArray->pData, stripsize) == 0) {
-				asynPrint(this->pasynUserSelf, ASYN_TRACE_ERROR, "TIFF Plugin: Trouble Writing data to file\n");
-			}
+            nwrite = TIFFWriteEncodedStrip( this->output, 0, (signed char *)pArray->pData, stripsize); 
             break;
         case NDInt16:
         case NDUInt16:
-			if ( TIFFWriteEncodedStrip( this->output, 0, (short *)pArray->pData, stripsize) == 0) {
-				asynPrint(this->pasynUserSelf, ASYN_TRACE_ERROR, "TIFF Plugin: Trouble Writing data to file\n");
-			}
+            nwrite = TIFFWriteEncodedStrip( this->output, 0, (short *)pArray->pData, stripsize);
             break;
         case NDInt32:
         case NDUInt32:
-			if ( TIFFWriteEncodedStrip( this->output, 0, (long int *)pArray->pData, stripsize) == 0) {
-				asynPrint(this->pasynUserSelf, ASYN_TRACE_ERROR, "TIFF Plugin: Trouble Writing data to file\n");
-			}
+            nwrite = TIFFWriteEncodedStrip( this->output, 0, (long int *)pArray->pData, stripsize);
             break;
         case NDFloat32:
-			printf( "NDFileTIFF does not support Floats\n");
-            break;
         case NDFloat64:
-			printf( "NDFileTIFF does not support Floats\n");
+            asynPrint(this->pasynUserSelf, ASYN_TRACE_ERROR, 
+                "%s:%s: floating point data not yet supported\n",
+                driverName, functionName);
+            return(asynError);
             break;
     }
+    if (nwrite <= 0) {
+        asynPrint(this->pasynUserSelf, ASYN_TRACE_ERROR, 
+            "%s:%s: error writing data to file\n",
+            driverName, functionName);
+        return(asynError);
+    }
 
-    this->nextRecord++;
     return(asynSuccess);
 }
 
@@ -159,26 +149,14 @@ asynStatus NDFileTIFF::readFile(NDArray **pArray)
 
 asynStatus NDFileTIFF::closeFile()
 {
-    static const char *functionName = "closeFile";
+    //static const char *functionName = "closeFile";
 
-    printf("Hello closeFile\n");
-		TIFFClose(this->output);
+    TIFFClose(this->output);
 
     return asynSuccess;
 }
 
 
-/* Configuration routine.  Called directly, or from the iocsh function in drvNDFileEpics */
-
-extern "C" int drvNDFileTIFFConfigure(const char *portName, int queueSize, int blockingCallbacks,
-                                     const char *NDArrayPort, int NDArrayAddr,
-                                     int priority, int stackSize)
-{
-    new NDFileTIFF(portName, queueSize, blockingCallbacks, NDArrayPort, NDArrayAddr,
-                     priority, stackSize);
-    return(asynSuccess);
-}
-
 /* The constructor for this class */
 NDFileTIFF::NDFileTIFF(const char *portName, int queueSize, int blockingCallbacks,
                            const char *NDArrayPort, int NDArrayAddr,
@@ -189,6 +167,44 @@ NDFileTIFF::NDFileTIFF(const char *portName, int queueSize, int blockingCallback
     //const char *functionName = "NDFileTIFF";
 
     this->supportsMultipleArrays = 0;
-    this->pAttributeId = NULL;
 }
 
+/* Configuration routine.  Called directly, or from the iocsh  */
+
+extern "C" int NDFileTIFFConfigure(const char *portName, int queueSize, int blockingCallbacks,
+                                   const char *NDArrayPort, int NDArrayAddr,
+                                   int priority, int stackSize)
+{
+    new NDFileTIFF(portName, queueSize, blockingCallbacks, NDArrayPort, NDArrayAddr,
+                     priority, stackSize);
+    return(asynSuccess);
+}
+
+
+/* EPICS iocsh shell commands */
+
+static const iocshArg initArg0 = { "portName",iocshArgString};
+static const iocshArg initArg1 = { "frame queue size",iocshArgInt};
+static const iocshArg initArg2 = { "blocking callbacks",iocshArgInt};
+static const iocshArg initArg3 = { "NDArray Port",iocshArgString};
+static const iocshArg initArg4 = { "NDArray Addr",iocshArgInt};
+static const iocshArg initArg5 = { "priority",iocshArgInt};
+static const iocshArg initArg6 = { "stack size",iocshArgInt};
+static const iocshArg * const initArgs[] = {&initArg0,
+                                            &initArg1,
+                                            &initArg2,
+                                            &initArg3,
+                                            &initArg4,
+                                            &initArg5,
+                                            &initArg6};
+static const iocshFuncDef initFuncDef = {"NDFileTIFFConfigure",7,initArgs};
+static void initCallFunc(const iocshArgBuf *args)
+{
+    NDFileTIFFConfigure(args[0].sval, args[1].ival, args[2].ival, args[3].sval, args[4].ival, args[5].ival, args[6].ival);
+}
+
+extern "C" void NDFileTIFFRegister(void)
+{
+    iocshRegister(&initFuncDef,initCallFunc);
+}
+epicsExportRegistrar(NDFileTIFFRegister);
