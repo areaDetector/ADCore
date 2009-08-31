f2afa3ae7de728e4a9f16e917ebf72a861a74773
rivers <rivers@dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b>
2009-08-31 21:19:02 +0000

    Fix error, was prosilicaApp rather than simDetectorApp
    git-svn-id: https://subversion.xor.aps.anl.gov/synApps/areaDetector/trunk@9460 dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b

--- iocBoot/iocSimDetector/start_epics.bat
+++ iocBoot/iocSimDetector/start_epics.bat
@@ -1,3 +1,4 @@
 start medm -x -macro "P=13PS1:, R=cam1:, I=image1:, ROI=ROI1:, NETCDF=netCDF1:, TIFF=TIFF1:, JPEG=JPEG1:, NEXUS=Nexus1:" prosilica.adl
-..\..\bin\win32-x86\prosilicaApp st.cmd.win32
+..\..\bin\win32-x86\simDetectorApp st.cmd.win32
+
 
