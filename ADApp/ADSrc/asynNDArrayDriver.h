1debcfde4761dcf9573b261f8d487f7367460ef0
rivers <rivers@dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b>
2009-11-23 23:09:38 +0000

    Changes for new drvInfo handling in asynPortDriver for asyn R4-13
    git-svn-id: https://subversion.xor.aps.anl.gov/synApps/areaDetector/trunk@9720 dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b

--- ADApp/ADSrc/asynNDArrayDriver.h
+++ ADApp/ADSrc/asynNDArrayDriver.h
@@ -18,26 +18,25 @@ typedef enum {
 } NDFileMode_t;
 
 
-/** Enumeration of parameters that affect the behaviour of the detector. 
-  * These are the values that asyn will place in pasynUser->reason when the
+/** Strings defining parameters that affect the behaviour of the detector. 
+  * These are the values passed to drvUserCreate. 
+  * The driver will place in pasynUser->reason an integer to be used when the
   * standard asyn interface methods are called. */
-typedef enum
-{
-    /*    Name          asyn interface  access   Description  */
-    NDPortNameSelf,     /**< (asynOctet,    r/o) Asyn port name of this driver instance */
+ /*                               String                 asyn interface  access   Description  */
+#define NDPortNameSelfString    "PORT_NAME_SELF"    /**< (asynOctet,    r/o) Asyn port name of this driver instance */
 
     /* Parameters defining the size of the array data from the detector.
      * NDArraySizeX and NDArraySizeY are the actual dimensions of the array data,
      * including effects of the region definition and binning */
-    NDArraySizeX,       /**< (asynInt32,    r/o) Size of the array data in the X direction */
-    NDArraySizeY,       /**< (asynInt32,    r/o) Size of the array data in the Y direction */
-    NDArraySizeZ,       /**< (asynInt32,    r/o) Size of the array data in the Z direction */
-    NDArraySize,        /**< (asynInt32,    r/o) Total size of array data in bytes */
-    NDDataType,         /**< (asynInt32,    r/w) Data type (NDDataType_t) */
-    NDColorMode,        /**< (asynInt32,    r/w) Color mode (NDColorMode_t) */
+#define NDArraySizeXString      "ARRAY_SIZE_X"      /**< (asynInt32,    r/o) Size of the array data in the X direction */
+#define NDArraySizeYString      "ARRAY_SIZE_Y"      /**< (asynInt32,    r/o) Size of the array data in the Y direction */
+#define NDArraySizeZString      "ARRAY_SIZE_Z"      /**< (asynInt32,    r/o) Size of the array data in the Z direction */
+#define NDArraySizeString       "ARRAY_SIZE"        /**< (asynInt32,    r/o) Total size of array data in bytes */
+#define NDDataTypeString        "DATA_TYPE"         /**< (asynInt32,    r/w) Data type (NDDataType_t) */
+#define NDColorModeString       "COLOR_MODE"        /**< (asynInt32,    r/w) Color mode (NDColorMode_t) */
 
     /* Statistics on number of arrays collected */
-    NDArrayCounter,     /**< (asynInt32,    r/w) Number of arrays since last reset */
+#define NDArrayCounterString    "ARRAY_COUNTER"     /**< (asynInt32,    r/w) Number of arrays since last reset */
 
     /* File name related parameters for saving data.
      * Drivers are not required to implement file saving, but if they do these parameters
@@ -45,33 +44,26 @@ typedef enum
      * The driver will normally combine NDFilePath, NDFileName, and NDFileNumber into
      * a file name that order using the format specification in NDFileTemplate.
      * For example NDFileTemplate might be "%s%s_%d.tif" */
-    NDFilePath,         /**< (asynOctet,    r/w) The file path */
-    NDFileName,         /**< (asynOctet,    r/w) The file name */
-    NDFileNumber,       /**< (asynInt32,    r/w) The next file number */
-    NDFileTemplate,     /**< (asynOctet,    r/w) The file format template; C-style format string */
-    NDAutoIncrement,    /**< (asynInt32,    r/w) Autoincrement file number; 0=No, 1=Yes */
-    NDFullFileName,     /**< (asynOctet,    r/o) The actual complete file name for the last file saved */
-    NDFileFormat,       /**< (asynInt32,    r/w) The data format to use for saving the file.  */
-    NDAutoSave,         /**< (asynInt32,    r/w) Automatically save files */
-    NDWriteFile,        /**< (asynInt32,    r/w) Manually save the most recent array to a file when value=1 */
-    NDReadFile,         /**< (asynInt32,    r/w) Manually read file when value=1 */
-    NDFileWriteMode,    /**< (asynInt32,    r/w) File saving mode (NDFileMode_t) */
-    NDFileNumCapture,   /**< (asynInt32,    r/w) Number of arrays to capture */
-    NDFileNumCaptured,  /**< (asynInt32,    r/o) Number of arrays already captured */
-    NDFileCapture,      /**< (asynInt32,    r/w) Start or stop capturing arrays */
+#define NDFilePathString        "FILE_PATH"         /**< (asynOctet,    r/w) The file path */
+#define NDFileNameString        "FILE_NAME"         /**< (asynOctet,    r/w) The file name */
+#define NDFileNumberString      "FILE_NUMBER"       /**< (asynInt32,    r/w) The next file number */
+#define NDFileTemplateString    "FILE_TEMPLATE"     /**< (asynOctet,    r/w) The file format template; C-style format string */
+#define NDAutoIncrementString   "AUTO_INCREMENT"    /**< (asynInt32,    r/w) Autoincrement file number; 0=No, 1=Yes */
+#define NDFullFileNameString    "FULL_FILE_NAME"    /**< (asynOctet,    r/o) The actual complete file name for the last file saved */
+#define NDFileFormatString      "FILE_FORMAT"       /**< (asynInt32,    r/w) The data format to use for saving the file.  */
+#define NDAutoSaveString        "AUTO_SAVE"         /**< (asynInt32,    r/w) Automatically save files */
+#define NDWriteFileString       "WRITE_FILE"        /**< (asynInt32,    r/w) Manually save the most recent array to a file when value=1 */
+#define NDReadFileString        "READ_FILE"         /**< (asynInt32,    r/w) Manually read file when value=1 */
+#define NDFileWriteModeString   "WRITE_MODE"        /**< (asynInt32,    r/w) File saving mode (NDFileMode_t) */
+#define NDFileNumCaptureString  "NUM_CAPTURE"       /**< (asynInt32,    r/w) Number of arrays to capture */
+#define NDFileNumCapturedString "NUM_CAPTURED"      /**< (asynInt32,    r/o) Number of arrays already captured */
+#define NDFileCaptureString     "CAPTURE"           /**< (asynInt32,    r/w) Start or stop capturing arrays */
 
-    NDAttributesFile,   /**< (asynOctet,    r/w) Attributes file name */
+#define NDAttributesFileString  "ND_ATTRIBUTES_FILE" /**< (asynOctet,    r/w) Attributes file name */
 
     /* The detector array data */
-    NDArrayData,        /**< (asynGenericPointer,   r/w) NDArray data */
-    NDArrayCallbacks,   /**< (asynInt32,    r/w) Do callbacks with array data (0=No, 1=Yes) */
-
-    NDLastStdParam      /**< The last standard ND driver parameter; 
-                          *  Derived classes must begin their specific parameter enums with this value */
-} NDStdDriverParam_t;
-
-#define NUM_ND_STANDARD_PARAMS (sizeof(NDStdDriverParamString)/sizeof(NDStdDriverParamString[0]))
-
+#define NDArrayDataString       "ARRAY_DATA"        /**< (asynGenericPointer,   r/w) NDArray data */
+#define NDArrayCallbacksString  "ARRAY_CALLBACKS"   /**< (asynInt32,    r/w) Do callbacks with array data (0=No, 1=Yes) */
 
 /** This is the class from which NDArray drivers are derived; implements the asynGenericPointer functions 
   * for NDArray objects. 
@@ -80,7 +72,7 @@ typedef enum
   */
 class asynNDArrayDriver : public asynPortDriver {
 public:
-    asynNDArrayDriver(const char *portName, int maxAddr, int paramTableSize, int maxBuffers, size_t maxMemory,
+    asynNDArrayDriver(const char *portName, int maxAddr, int numParams, int maxBuffers, size_t maxMemory,
                       int interfaceMask, int interruptMask,
                       int asynFlags, int autoConnect, int priority, int stackSize);
 
@@ -89,8 +81,6 @@ public:
                           size_t *nActual);
     virtual asynStatus readGenericPointer(asynUser *pasynUser, void *genericPointer);
     virtual asynStatus writeGenericPointer(asynUser *pasynUser, void *genericPointer);
-    virtual asynStatus drvUserCreate(asynUser *pasynUser, const char *drvInfo,
-                                     const char **pptypeName, size_t *psize);
     virtual void report(FILE *fp, int details);
 
     /* These are the methods that are new to this class */
@@ -100,10 +90,39 @@ public:
     virtual int getAttributes(NDAttributeList *pAttributeList);
 
 protected:
+    int NDPortNameSelf;
+    #define FIRST_NDARRAY_PARAM NDPortNameSelf
+    int NDArraySizeX;
+    int NDArraySizeY;
+    int NDArraySizeZ;
+    int NDArraySize;
+    int NDDataType;
+    int NDColorMode;
+    int NDArrayCounter;
+    int NDFilePath;
+    int NDFileName;
+    int NDFileNumber;
+    int NDFileTemplate;
+    int NDAutoIncrement;
+    int NDFullFileName;
+    int NDFileFormat;
+    int NDAutoSave;
+    int NDWriteFile;
+    int NDReadFile;
+    int NDFileWriteMode;
+    int NDFileNumCapture;
+    int NDFileNumCaptured;
+    int NDFileCapture;   
+    int NDAttributesFile;
+    int NDArrayData;
+    int NDArrayCallbacks;
+    #define LAST_NDARRAY_PARAM NDArrayCallbacks
+
     NDArray **pArrays;             /**< An array of NDArray pointers used to store data in the driver */
     NDArrayPool *pNDArrayPool;     /**< An NDArrayPool object used to allocate and manipulate NDArray objects */
     class NDAttributeList *pAttributeList;  /**< An NDAttributeList object used to obtain the current values of a set of
                                           *  attributes */
 };
 
+#define NUM_NDARRAY_PARAMS (&LAST_NDARRAY_PARAM - &FIRST_NDARRAY_PARAM + 1)
 #endif
