bb794cd495c89df4f16d765cf888eb3325ee3f88
rivers <rivers@dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b>
2009-11-23 23:12:41 +0000

    Changes for new drvInfo handling in asynPortDriver for asyn R4-13
    git-svn-id: https://subversion.xor.aps.anl.gov/synApps/areaDetector/trunk@9723 dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b

--- ADApp/pluginSrc/NDPluginROI.h
+++ ADApp/pluginSrc/NDPluginROI.h
@@ -24,60 +24,49 @@ typedef struct NDROI {
     double *histogram;
 } NDROI_t;
 
-/* The following enum is for each of the ROIs */
-#define NDPluginROIFirstROINParam NDPluginDriverLastParam
-
-/** Enums for plugin-specific parameters. */
-typedef enum {
-    NDPluginROIName                    /* (asynOctet,   r/w) Name of this ROI */
-        = NDPluginROIFirstROINParam,
-    NDPluginROIUse,                    /* (asynInt32,   r/w) Use this ROI? */
-    NDPluginROIComputeStatistics,      /* (asynInt32,   r/w) Compute statistics for this ROI? */
-    NDPluginROIComputeHistogram,       /* (asynInt32,   r/w) Compute histogram for this ROI? */
-    NDPluginROIComputeProfiles,        /* (asynInt32,   r/w) Compute profiles for this ROI? */
-    NDPluginROIHighlight,              /* (asynInt32,   r/w) Highlight ROIs? */
+#define NDPluginROINameString               "NAME"               /* (asynOctet,   r/w) Name of this ROI */
+#define NDPluginROIUseString                "USE"                /* (asynInt32,   r/w) Use this ROI? */
+#define NDPluginROIComputeStatisticsString  "COMPUTE_STATISTICS" /* (asynInt32,   r/w) Compute statistics for this ROI? */
+#define NDPluginROIComputeHistogramString   "COMPUTE_HISTOGRAM" /* (asynInt32,   r/w) Compute histogram for this ROI? */
+#define NDPluginROIComputeProfilesString    "COMPUTE_PROFILES"  /* (asynInt32,   r/w) Compute profiles for this ROI? */
+#define NDPluginROIHighlightString          "HIGHLIGHT"         /* (asynInt32,   r/w) Highlight ROIs? */
     
     /* ROI definition */
-    NDPluginROIDim0Min,                /* (asynInt32,   r/w) Starting element of ROI in each dimension */
-    NDPluginROIDim0Size,               /* (asynInt32,   r/w) Size of ROI in each dimension */
-    NDPluginROIDim0MaxSize,            /* (asynInt32,   r/o) Maximum size of ROI in each dimension */
-    NDPluginROIDim0Bin,                /* (asynInt32,   r/w) Binning of ROI in each dimension */
-    NDPluginROIDim0Reverse,            /* (asynInt32,   r/w) Reversal of ROI in each dimension */
-    NDPluginROIDim1Min,                /* (asynInt32,   r/w) Starting element of ROI in each dimension */
-    NDPluginROIDim1Size,               /* (asynInt32,   r/w) Size of ROI in each dimension */
-    NDPluginROIDim1MaxSize,            /* (asynInt32,   r/o) Maximum size of ROI in each dimension */
-    NDPluginROIDim1Bin,                /* (asynInt32,   r/w) Binning of ROI in each dimension */
-    NDPluginROIDim1Reverse,            /* (asynInt32,   r/w) Reversal of ROI in each dimension */
-    NDPluginROIDim2Min,                /* (asynInt32,   r/w) Starting element of ROI in each dimension */
-    NDPluginROIDim2Size,               /* (asynInt32,   r/w) Size of ROI in each dimension */
-    NDPluginROIDim2MaxSize,            /* (asynInt32,   r/o) Maximum size of ROI in each dimension */
-    NDPluginROIDim2Bin,                /* (asynInt32,   r/w) Binning of ROI in each dimension */
-    NDPluginROIDim2Reverse,            /* (asynInt32,   r/w) Reversal of ROI in each dimension */
-    NDPluginROIDataType,               /* (asynInt32,   r/w) Data type for ROI.  -1 means automatic. */
+#define NDPluginROIDim0MinString            "DIM0_MIN"          /* (asynInt32,   r/w) Starting element of ROI in each dimension */
+#define NDPluginROIDim0SizeString           "DIM0_SIZE"         /* (asynInt32,   r/w) Size of ROI in each dimension */
+#define NDPluginROIDim0MaxSizeString        "DIM0_MAX_SIZE"     /* (asynInt32,   r/o) Maximum size of ROI in each dimension */
+#define NDPluginROIDim0BinString            "DIM0_BIN"          /* (asynInt32,   r/w) Binning of ROI in each dimension */
+#define NDPluginROIDim0ReverseString        "DIM0_REVERSE"      /* (asynInt32,   r/w) Reversal of ROI in each dimension */
+#define NDPluginROIDim1MinString            "DIM1_MIN"          /* (asynInt32,   r/w) Starting element of ROI in each dimension */
+#define NDPluginROIDim1SizeString           "DIM1_SIZE"         /* (asynInt32,   r/w) Size of ROI in each dimension */
+#define NDPluginROIDim1MaxSizeString        "DIM1_MAX_SIZE"     /* (asynInt32,   r/o) Maximum size of ROI in each dimension */
+#define NDPluginROIDim1BinString            "DIM1_BIN"          /* (asynInt32,   r/w) Binning of ROI in each dimension */
+#define NDPluginROIDim1ReverseString        "DIM1_REVERSE"      /* (asynInt32,   r/w) Reversal of ROI in each dimension */
+#define NDPluginROIDim2MinString            "DIM2_MIN"          /* (asynInt32,   r/w) Starting element of ROI in each dimension */
+#define NDPluginROIDim2SizeString           "DIM2_SIZE"         /* (asynInt32,   r/w) Size of ROI in each dimension */
+#define NDPluginROIDim2MaxSizeString        "DIM2_MAX_SIZE"     /* (asynInt32,   r/o) Maximum size of ROI in each dimension */
+#define NDPluginROIDim2BinString            "DIM2_BIN"          /* (asynInt32,   r/w) Binning of ROI in each dimension */
+#define NDPluginROIDim2ReverseString        "DIM2_REVERSE"      /* (asynInt32,   r/w) Reversal of ROI in each dimension */
+#define NDPluginROIDataTypeString           "ROI_DATA_TYPE"     /* (asynInt32,   r/w) Data type for ROI.  -1 means automatic. */
     
     /* ROI statistics */
-    NDPluginROIBgdWidth,               /* (asynInt32,   r/w) Width of background region when computing net */
-    NDPluginROIMinValue,               /* (asynFloat64, r/o) Minimum counts in any element */
-    NDPluginROIMaxValue,               /* (asynFloat64, r/o) Maximum counts in any element */
-    NDPluginROIMeanValue,              /* (asynFloat64, r/o) Mean counts of all elements */
-    NDPluginROITotal,                  /* (asynFloat64, r/o) Sum of all elements */
-    NDPluginROINet,                    /* (asynFloat64, r/o) Sum of all elements minus background */
+#define NDPluginROIBgdWidthString           "BGD_WIDTH"         /* (asynInt32,   r/w) Width of background region when computing net */
+#define NDPluginROIMinValueString           "MIN_VALUE"         /* (asynFloat64, r/o) Minimum counts in any element */
+#define NDPluginROIMaxValueString           "MAX_VALUE"         /* (asynFloat64, r/o) Maximum counts in any element */
+#define NDPluginROIMeanValueString          "MEAN_VALUE"        /* (asynFloat64, r/o) Mean counts of all elements */
+#define NDPluginROITotalString              "TOTAL"             /* (asynFloat64, r/o) Sum of all elements */
+#define NDPluginROINetString                "NET"               /* (asynFloat64, r/o) Sum of all elements minus background */
     
     /* ROI histogram */
-    NDPluginROIHistSize,               /* (asynInt32,   r/w) Number of elements in histogram */
-    NDPluginROIHistMin,                /* (asynFloat64, r/w) Minimum value for histogram */
-    NDPluginROIHistMax,                /* (asynFloat64, r/w) Maximum value for histogram */
-    NDPluginROIHistEntropy,            /* (asynFloat64, r/o) Image entropy calculcated from histogram */
-    NDPluginROIHistArray,              /* (asynFloat64Array, r/o) Histogram array */
+#define NDPluginROIHistSizeString           "HIST_SIZE"         /* (asynInt32,   r/w) Number of elements in histogram */
+#define NDPluginROIHistMinString            "HIST_MIN"          /* (asynFloat64, r/w) Minimum value for histogram */
+#define NDPluginROIHistMaxString            "HIST_MAX"          /* (asynFloat64, r/w) Maximum value for histogram */
+#define NDPluginROIHistEntropyString        "HIST_ENTROPY"      /* (asynFloat64, r/o) Image entropy calculcated from histogram */
+#define NDPluginROIHistArrayString          "HIST_ARRAY"        /* (asynFloat64Array, r/o) Histogram array */
 
-    NDPluginROITotalArray,             /* (asynInt32Array, r/o) Total counts array */
-    NDPluginROINetArray,               /* (asynInt32Array, r/o) Net counts array */
+#define NDPluginROITotalArrayString         "TOTAL_ARRAY"       /* (asynInt32Array, r/o) Total counts array */
+#define NDPluginROINetArrayString           "NET_ARRAY"         /* (asynInt32Array, r/o) Net counts array */
 
-    NDPluginROILastROINParam
-} NDPluginROINParam_t;
-
-
-#define NUM_ROIN_PARAMS (sizeof(NDPluginROINParamString)/sizeof(NDPluginROINParamString[0]))
 
 /** Extract Regions-Of-Interest (ROI) from NDArray data; the plugin can be a source of NDArray callbacks for
   * other plugins, passing these sub-arrays. 
@@ -91,12 +80,48 @@ public:
     /* These methods override the virtual methods in the base class */
     void processCallbacks(NDArray *pArray);
     asynStatus writeInt32(asynUser *pasynUser, epicsInt32 value);
-    asynStatus drvUserCreate(asynUser *pasynUser, const char *drvInfo, 
-                             const char **pptypeName, size_t *psize);
 
     /* These methods are unique to this class */
     asynStatus readFloat64Array(asynUser *pasynUser, epicsFloat64 *value, 
                                 size_t nelements, size_t *nIn);
+protected:
+    int NDPluginROIName;
+    #define FIRST_NDPLUGIN_ROI_PARAM NDPluginROIName
+    int NDPluginROIUse;
+    int NDPluginROIComputeStatistics;
+    int NDPluginROIComputeHistogram;
+    int NDPluginROIComputeProfiles;
+    int NDPluginROIHighlight;
+    int NDPluginROIDim0Min;
+    int NDPluginROIDim0Size;
+    int NDPluginROIDim0MaxSize;
+    int NDPluginROIDim0Bin;
+    int NDPluginROIDim0Reverse;
+    int NDPluginROIDim1Min;
+    int NDPluginROIDim1Size;
+    int NDPluginROIDim1MaxSize;
+    int NDPluginROIDim1Bin;
+    int NDPluginROIDim1Reverse;
+    int NDPluginROIDim2Min;
+    int NDPluginROIDim2Size;
+    int NDPluginROIDim2MaxSize;
+    int NDPluginROIDim2Bin;
+    int NDPluginROIDim2Reverse;
+    int NDPluginROIDataType;
+    int NDPluginROIBgdWidth;
+    int NDPluginROIMinValue;
+    int NDPluginROIMaxValue;
+    int NDPluginROIMeanValue;
+    int NDPluginROITotal;
+    int NDPluginROINet;
+    int NDPluginROITotalArray;
+    int NDPluginROINetArray;
+    int NDPluginROIHistSize;
+    int NDPluginROIHistMin;
+    int NDPluginROIHistMax;
+    int NDPluginROIHistEntropy;
+    int NDPluginROIHistArray;
+    #define LAST_NDPLUGIN_ROI_PARAM NDPluginROIHistArray
                                 
 private:
     int maxROIs;
@@ -104,5 +129,6 @@ private:
     epicsInt32 *totalArray;
     epicsInt32 *netArray;
 };
+#define NUM_NDPLUGIN_ROI_PARAMS (&LAST_NDPLUGIN_ROI_PARAM - &FIRST_NDPLUGIN_ROI_PARAM + 1)
     
 #endif
