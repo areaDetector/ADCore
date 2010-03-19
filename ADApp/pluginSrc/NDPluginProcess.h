1ed4c07ba4d52eaf1b96e0b145ff3f9b8fd2d90f
rivers <rivers@dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b>
2010-03-19 16:00:18 +0000

    Added support for recursive filter; change code so it can do the heavy calculations with the lock released, greatly improves driver performance
    git-svn-id: https://subversion.xor.aps.anl.gov/synApps/areaDetector/trunk@10277 dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b

--- ADApp/pluginSrc/NDPluginProcess.h
+++ ADApp/pluginSrc/NDPluginProcess.h
@@ -7,15 +7,15 @@
 #include "NDPluginDriver.h"
 
 /* Background array subtraction */
-#define NDPluginProcessSaveBackgroundString     "SAVE_BACKGROUND"     /* (asynInt32,   r/w) Save the current frame as background */
-#define NDPluginProcessEnableBackgroundString   "ENABLE_BACKGROUND"   /* (asynInt32,   r/w) Enable background subtraction? */
-#define NDPluginProcessValidBackgroundString    "VALID_BACKGROUND"    /* (asynInt32,   r/o) Is there a valid background */
+#define NDPluginProcessSaveBackgroundString     "SAVE_BACKGROUND"   /* (asynInt32,   r/w) Save the current frame as background */
+#define NDPluginProcessEnableBackgroundString   "ENABLE_BACKGROUND" /* (asynInt32,   r/w) Enable background subtraction? */
+#define NDPluginProcessValidBackgroundString    "VALID_BACKGROUND"  /* (asynInt32,   r/o) Is there a valid background */
 
 /* Flat field normalization */
-#define NDPluginProcessSaveFlatFieldString      "SAVE_FLAT_FIELD"     /* (asynInt32,   r/w) Save the current frame as flat field */
-#define NDPluginProcessEnableFlatFieldString    "ENABLE_FLAT_FIELD"   /* (asynInt32,   r/w) Enable flat field normalization? */
-#define NDPluginProcessValidFlatFieldString     "VALID_FLAT_FIELD"    /* (asynInt32,   r/o) Is there a valid flat field */
-#define NDPluginProcessScaleFlatFieldString     "SCALE_FLAT_FIELD"    /* (asynInt32,   r/o) Scale factor after dividing by flat field */
+#define NDPluginProcessSaveFlatFieldString      "SAVE_FLAT_FIELD"   /* (asynInt32,   r/w) Save the current frame as flat field */
+#define NDPluginProcessEnableFlatFieldString    "ENABLE_FLAT_FIELD" /* (asynInt32,   r/w) Enable flat field normalization? */
+#define NDPluginProcessValidFlatFieldString     "VALID_FLAT_FIELD"  /* (asynInt32,   r/o) Is there a valid flat field */
+#define NDPluginProcessScaleFlatFieldString     "SCALE_FLAT_FIELD"  /* (asynInt32,   r/o) Scale factor after dividing by flat field */
 
 /* High and low clipping */
 #define NDPluginProcessLowClipString            "LOW_CLIP"          /* (asynFloat64, r/w) Low clip value */
@@ -23,13 +23,25 @@
 #define NDPluginProcessHighClipString           "HIGH_CLIP"         /* (asynFloat64, r/w) High clip value */
 #define NDPluginProcessEnableHighClipString     "ENABLE_HIGH_CLIP"  /* (asynInt32,   r/w) Enable high clipping? */
     
-/* Frame averaging */
-#define NDPluginProcessEnableAverageString      "ENABLE_AVERAGE"      /* (asynInt32,   r/w) Enable frame averaging? */
-#define NDPluginProcessNumAverageString         "NUM_AVERAGE"         /* (asynInt32,   r/o) Number of frames to average */
-#define NDPluginProcessNumAveragedString        "NUM_AVERAGED"        /* (asynInt32,   r/o) Number of frames in current average */
+/* Recursive filter */
+#define NDPluginProcessEnableFilterString       "ENABLE_FILTER"     /* (asynInt32,   r/w) Enable frame filtering? */
+#define NDPluginProcessNumFilterString          "NUM_FILTER"        /* (asynInt32,   r/w) Number of frames to filter */
+#define NDPluginProcessNumFilteredString        "NUM_FILTERED"      /* (asynInt32,   r/o) Number of frames filtered */
+#define NDPluginProcessOC0String                "FILTER_OC0"        /* (asynFloat64, r/w) Output coefficient 0 */
+#define NDPluginProcessOC1String                "FILTER_OC1"        /* (asynFloat64, r/w) Output coefficient 1 */
+#define NDPluginProcessOC2String                "FILTER_OC2"        /* (asynFloat64, r/w) Output coefficient 2 */
+#define NDPluginProcessOC3String                "FILTER_OC3"        /* (asynFloat64, r/w) Output coefficient 3 */
+#define NDPluginProcessOC4String                "FILTER_OC4"        /* (asynFloat64, r/w) Output coefficient 4 */
+#define NDPluginProcessFC0String                "FILTER_FC0"        /* (asynFloat64, r/w) Filter coefficient 0 */
+#define NDPluginProcessFC1String                "FILTER_FC1"        /* (asynFloat64, r/w) Filter coefficient 1 */
+#define NDPluginProcessFC2String                "FILTER_FC2"        /* (asynFloat64, r/w) Filter coefficient 2 */
+#define NDPluginProcessFC3String                "FILTER_FC3"        /* (asynFloat64, r/w) Filter coefficient 3 */
+#define NDPluginProcessFC4String                "FILTER_FC4"        /* (asynFloat64, r/w) Filter coefficient 4 */
+#define NDPluginProcessRC0String                "FILTER_RC0"        /* (asynFloat64, r/w) Filter coefficient 5 */
+#define NDPluginProcessRC1String                "FILTER_RC1"        /* (asynFloat64, r/w) Filter coefficient 6 */
 
 /* Output data type */
-#define NDPluginProcessDataTypeString           "PROCESS_DATA_TYPE"   /* (asynInt32,   r/w) Output type.  -1 means automatic. */
+#define NDPluginProcessDataTypeString           "PROCESS_DATA_TYPE" /* (asynInt32,   r/w) Output type.  -1 means automatic. */
    
 
 /** Does image processing operations.  These include
@@ -67,10 +79,22 @@ protected:
     int NDPluginProcessHighClip;
     int NDPluginProcessEnableHighClip;
 
-    /* Frame averaging */
-    int NDPluginProcessEnableAverage;
-    int NDPluginProcessNumAverage;
-    int NDPluginProcessNumAveraged;
+    /* Frame filtering */
+    int NDPluginProcessEnableFilter;
+    int NDPluginProcessNumFilter;
+    int NDPluginProcessNumFiltered;
+    int NDPluginProcessOC0;
+    int NDPluginProcessOC1;
+    int NDPluginProcessOC2;
+    int NDPluginProcessOC3;
+    int NDPluginProcessOC4;
+    int NDPluginProcessFC0;
+    int NDPluginProcessFC1;
+    int NDPluginProcessFC2;
+    int NDPluginProcessFC3;
+    int NDPluginProcessFC4;
+    int NDPluginProcessRC0;
+    int NDPluginProcessRC1;
     
     /* Output data type */
     int NDPluginProcessDataType;
@@ -79,11 +103,14 @@ protected:
                                 
 private:
     NDArray *pBackground;
+    int     newBackground;
     int     nBackgroundElements;
     NDArray *pFlatField;
+    int     newFlatField;
     int     nFlatFieldElements;
-    NDArray *pAverage;
-    int     numAveraged;
+    NDArray *pFilter;
+    int     newFilter;
+    int     numFiltered;
 };
 #define NUM_NDPLUGIN_PROCESS_PARAMS (&LAST_NDPLUGIN_PROCESS_PARAM - &FIRST_NDPLUGIN_PROCESS_PARAM + 1)
     
