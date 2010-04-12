b9d076af145c1ed5017719b3f27fa42b9cddb21c
rivers <rivers@dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b>
2010-04-12 17:08:09 +0000

    New file for vxWorks so it can use same dbd file
    git-svn-id: https://subversion.xor.aps.anl.gov/synApps/areaDetector/trunk@10627 dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b

---
+++ ADApp/pluginSrc/NDFileDummy.cpp
@@ -0,0 +1,25 @@
+/* This is a dummy routine for systems like vxWorks that don't support JPEG, Nexus or TIFF */
+
+#include <epicsExport.h>
+
+extern "C" void NDFileJPEGRegister(void)
+{
+}
+extern "C" {
+epicsExportRegistrar(NDFileJPEGRegister);
+}
+
+extern "C" void NDFileNexusRegister(void)
+{
+}
+extern "C" {
+epicsExportRegistrar(NDFileNexusRegister);
+}
+
+
+extern "C" void NDFileTiffRegister(void)
+{
+}
+extern "C" {
+epicsExportRegistrar(NDFileTiffRegister);
+}
