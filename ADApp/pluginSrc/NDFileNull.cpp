14790b27129564531b2db2a40538c82664f6f57b
rivers <rivers@dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b>
2011-11-30 17:50:16 +0000

    New Null (dummy) file writing plugin
    git-svn-id: https://subversion.xor.aps.anl.gov/synApps/areaDetector/trunk@14181 dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b

---
+++ ADApp/pluginSrc/NDFileNull.cpp
@@ -0,0 +1,134 @@
+/* NDFileNull.cpp
+ * Dummy file writer, whose main purpose is to allow deleting original driver files without re-writing them in 
+ * an actual file plugin.
+ *
+ * Mark Rivers
+ * November 30, 2011
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <epicsStdio.h>
+#include <iocsh.h>
+
+#include <epicsExport.h>
+
+#include "NDFileNull.h"
+
+static const char *driverName = "NDFileNull";
+
+#define MAX_ATTRIBUTE_STRING_SIZE 256
+
+/** Opens a Null file.
+  * \param[in] fileName The name of the file to open.
+  * \param[in] openMode Mask defining how the file should be opened; bits are 
+  *            NDFileModeRead, NDFileModeWrite, NDFileModeAppend, NDFileModeMultiple
+  * \param[in] pArray A pointer to an NDArray; this is used to determine the array and attribute properties.
+  */
+asynStatus NDFileNull::openFile(const char *fileName, NDFileOpenMode_t openMode, NDArray *pArray)
+{
+    return(asynSuccess);
+}
+
+/** Writes single NDArray to the Null file.
+  * \param[in] pArray Pointer to the NDArray to be written
+  */
+asynStatus NDFileNull::writeFile(NDArray *pArray)
+{
+    return(asynSuccess);
+}
+
+/** Reads single NDArray from a Null file; NOT CURRENTLY IMPLEMENTED.
+  * \param[in] pArray Pointer to the NDArray to be read
+  */
+asynStatus NDFileNull::readFile(NDArray **pArray)
+{
+    return asynError;
+}
+
+
+/** Closes the Null file. */
+asynStatus NDFileNull::closeFile()
+{
+    return asynSuccess;
+}
+
+
+/** Constructor for NDFileNull; all parameters are simply passed to NDPluginFile::NDPluginFile.
+  * \param[in] portName The name of the asyn port driver to be created.
+  * \param[in] queueSize The number of NDArrays that the input queue for this plugin can hold when 
+  *            NDPluginDriverBlockingCallbacks=0.  Larger queues can decrease the number of dropped arrays,
+  *            at the expense of more NDArray buffers being allocated from the underlying driver's NDArrayPool.
+  * \param[in] blockingCallbacks Initial setting for the NDPluginDriverBlockingCallbacks flag.
+  *            0=callbacks are queued and executed by the callback thread; 1 callbacks execute in the thread
+  *            of the driver doing the callbacks.
+  * \param[in] NDArrayPort Name of asyn port driver for initial source of NDArray callbacks.
+  * \param[in] NDArrayAddr asyn port driver address for initial source of NDArray callbacks.
+  * \param[in] priority The thread priority for the asyn port driver thread if ASYN_CANBLOCK is set in asynFlags.
+  * \param[in] stackSize The stack size for the asyn port driver thread if ASYN_CANBLOCK is set in asynFlags.
+  */
+NDFileNull::NDFileNull(const char *portName, int queueSize, int blockingCallbacks,
+                       const char *NDArrayPort, int NDArrayAddr,
+                       int priority, int stackSize)
+    /* Invoke the base class constructor.
+     * We allocate 2 NDArrays of unlimited size in the NDArray pool.
+     * This driver can block (because writing a file can be slow), and it is not multi-device.  
+     * Set autoconnect to 1.  priority and stacksize can be 0, which will use defaults. */
+    : NDPluginFile(portName, queueSize, blockingCallbacks,
+                   NDArrayPort, NDArrayAddr, 1, NUM_NDFILE_NULL_PARAMS,
+                   2, -1, asynGenericPointerMask, asynGenericPointerMask, 
+                   ASYN_CANBLOCK, 1, priority, stackSize)
+{
+    //const char *functionName = "NDFileNull";
+
+    /* Set the plugin type string */    
+    setStringParam(NDPluginDriverPluginType, "NDFileNull");
+    this->supportsMultipleArrays = 0;
+}
+
+/* Configuration routine.  Called directly, or from the iocsh  */
+
+extern "C" int NDFileNullConfigure(const char *portName, int queueSize, int blockingCallbacks,
+                                   const char *NDArrayPort, int NDArrayAddr,
+                                   int priority, int stackSize)
+{
+    NDFileNull *pPlugin = 
+        new NDFileNull(portName, queueSize, blockingCallbacks, NDArrayPort, NDArrayAddr,
+                       priority, stackSize);
+    pPlugin = NULL;  /* This is just to eliminate compiler warning about unused variables/objects */
+    return(asynSuccess);
+}
+
+
+/* EPICS iocsh shell commands */
+
+static const iocshArg initArg0 = { "portName",iocshArgString};
+static const iocshArg initArg1 = { "frame queue size",iocshArgInt};
+static const iocshArg initArg2 = { "blocking callbacks",iocshArgInt};
+static const iocshArg initArg3 = { "NDArray Port",iocshArgString};
+static const iocshArg initArg4 = { "NDArray Addr",iocshArgInt};
+static const iocshArg initArg5 = { "priority",iocshArgInt};
+static const iocshArg initArg6 = { "stack size",iocshArgInt};
+static const iocshArg * const initArgs[] = {&initArg0,
+                                            &initArg1,
+                                            &initArg2,
+                                            &initArg3,
+                                            &initArg4,
+                                            &initArg5,
+                                            &initArg6};
+static const iocshFuncDef initFuncDef = {"NDFileNullConfigure",7,initArgs};
+static void initCallFunc(const iocshArgBuf *args)
+{
+    NDFileNullConfigure(args[0].sval, args[1].ival, args[2].ival, args[3].sval, args[4].ival, args[5].ival, args[6].ival);
+}
+
+extern "C" void NDFileNullRegister(void)
+{
+    iocshRegister(&initFuncDef,initCallFunc);
+}
+
+extern "C" {
+epicsExportRegistrar(NDFileNullRegister);
+}
---
+++ ADApp/pluginSrc/NDFileNull.h
@@ -0,0 +1,30 @@
+/*
+ * NDFileNull.h
+ * Dummy file writer, whose main purpose is to allow deleting original driver files without re-writing them in 
+ * an actual file plugin.
+ *
+ * Mark Rivers
+ * November 30, 2011
+ */
+
+#ifndef DRV_NDFileNULL_H
+#define DRV_NDFileNULL_H
+
+#include "NDPluginFile.h"
+
+/** Writes NDArrays in the Null file format. */
+
+class NDFileNull : public NDPluginFile {
+public:
+    NDFileNull(const char *portName, int queueSize, int blockingCallbacks,
+                 const char *NDArrayPort, int NDArrayAddr,
+                 int priority, int stackSize);
+
+    /* The methods that this class implements */
+    virtual asynStatus openFile(const char *fileName, NDFileOpenMode_t openMode, NDArray *pArray);
+    virtual asynStatus readFile(NDArray **pArray);
+    virtual asynStatus writeFile(NDArray *pArray);
+    virtual asynStatus closeFile();
+};
+#define NUM_NDFILE_NULL_PARAMS 0
+#endif
