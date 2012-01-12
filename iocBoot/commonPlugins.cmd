f82b136308c0f29b9822d9a5929958e24347befe
rivers <rivers@dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b>
2012-01-12 22:58:52 +0000

    Increased stack size on NFFileNexus plugin, it was crashing on windows-x64
    git-svn-id: https://subversion.xor.aps.anl.gov/synApps/areaDetector/trunk@14358 dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b

--- iocBoot/commonPlugins.cmd
+++ iocBoot/commonPlugins.cmd
@@ -17,7 +17,7 @@ dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDFile.template",      "P=$(PREFIX),R=J
 dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDFileJPEG.template",  "P=$(PREFIX),R=JPEG1:,PORT=FileJPEG1,ADDR=0,TIMEOUT=1")
 
 # Create a NeXus file saving plugin
-NDFileNexusConfigure("FileNexus1", $(QSIZE), 0, "$(PORT)", 0)
+NDFileNexusConfigure("FileNexus1", $(QSIZE), 0, "$(PORT)", 0, 0, 100000)
 dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDPluginBase.template","P=$(PREFIX),R=Nexus1:,PORT=FileNexus1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT),NDARRAY_ADDR=0")
 dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDFile.template",      "P=$(PREFIX),R=Nexus1:,PORT=FileNexus1,ADDR=0,TIMEOUT=1")
 dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDFileNexus.template", "P=$(PREFIX),R=Nexus1:,PORT=FileNexus1,ADDR=0,TIMEOUT=1")
