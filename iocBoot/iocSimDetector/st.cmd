df5eee6aff131dbb51380a6833eb6d7ada60fb79
rivers <rivers@dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b>
2012-07-25 14:33:30 +0000

    Added roi_sync and yySseq
    git-svn-id: https://subversion.xor.aps.anl.gov/synApps/areaDetector/trunk@14905 dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b

--- iocBoot/iocSimDetector/st.cmd
+++ iocBoot/iocSimDetector/st.cmd
@@ -8,8 +8,8 @@ simDetectorApp_registerRecordDeviceDriver(pdbbase)
 epicsEnvSet("PREFIX", "13SIM1:")
 epicsEnvSet("PORT",   "SIM1")
 epicsEnvSet("QSIZE",  "20")
-epicsEnvSet("XSIZE",  "640")
-epicsEnvSet("YSIZE",  "480")
+epicsEnvSet("XSIZE",  "1024")
+epicsEnvSet("YSIZE",  "1024")
 epicsEnvSet("NCHANS", "2048")
 
 # Create a simDetector driver
@@ -28,7 +28,7 @@ dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDPluginBase.template","P=$(PREFIX),R=i
 
 # This creates a waveform large enough for 640x480x3 (e.g. RGB color) arrays.
 # This waveform only allows transporting 8-bit images
-dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDStdArrays.template", "P=$(PREFIX),R=image1:,PORT=Image1,ADDR=0,TIMEOUT=1,TYPE=Int8,FTVL=UCHAR,NELEMENTS=921600")
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDStdArrays.template", "P=$(PREFIX),R=image1:,PORT=Image1,ADDR=0,TIMEOUT=1,TYPE=Int16,FTVL=USHORT,NELEMENTS=921600")
 # This waveform allows transporting 32-bit images
 #dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDStdArrays.template", "P=$(PREFIX),R=image1:,PORT=Image1,ADDR=0,TIMEOUT=1,TYPE=Int32,FTVL=LONG,NELEMENTS=921600")
 
@@ -40,8 +40,16 @@ dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDPluginBase.template","P=$(PREFIX),R=i
 # This waveform allows transporting 64-bit images, so it can handle any detector data type at the expense of more memory and bandwidth
 dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDStdArrays.template", "P=$(PREFIX),R=image2:,PORT=Image2,ADDR=0,TIMEOUT=1,TYPE=Float64,FTVL=DOUBLE,NELEMENTS=921600")
 
+
+# This loads a database to tie the ROI size to the detector readout size
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/roi_sync.template", "P=$(PREFIX),CAM=cam1:,ROI=ROI1:")
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/roi_sync.template", "P=$(PREFIX),CAM=cam1:,ROI=ROI2:")
+
 # Load all other plugins using commonPlugins.cmd
 < ../commonPlugins.cmd
+
+# Load sseq record for acquisition sequence
+dbLoadRecords("$(CALC)/calcApp/Db/yySseq.db", "P=$(PREFIX), S=AcquireSequence")
 
 #asynSetTraceIOMask("$(PORT)",0,2)
 #asynSetTraceMask("$(PORT)",0,255)
