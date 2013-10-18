a83cf5d6b7a1ab89d8a54b0aadaa5f7e1d7ec31e
rivers <rivers@dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b>
2013-10-18 21:21:44 +0000

    Use new functionPvt pointer
    git-svn-id: https://subversion.xor.aps.anl.gov/synApps/areaDetector/trunk@17041 dc6c5ff5-0b8b-c028-a01f-ffb33f00fc8b

--- ADApp/ADSrc/myAttributeFunctions.cpp
+++ ADApp/ADSrc/myAttributeFunctions.cpp
@@ -1,5 +1,6 @@
 #include <math.h>
 #include <string.h>
+#include <stdlib.h>
 #include <registryFunction.h>
 #include <epicsExport.h>
 #include "functAttribute.h"
@@ -9,25 +10,65 @@
 static double _PI = atan(1.0)*4;
 static double _E = exp(1.0);
 
-static int myAttrFunct1(const char *paramString, functAttribute *pAttribute)
+typedef enum {
+    functPi,
+    functE,
+    functTen,
+    functGettysburg
+} myFunct_t;
+
+static int myAttrFunct1(const char *paramString, void **functionPvt, functAttribute *pAttribute)
 {
     int ten = 10;
     const char *gettysburg = "Four score and seven years ago our fathers";
-
-    if (!strcmp(paramString, "PI")) {
-        pAttribute->setValue(NDAttrFloat64, &_PI);
+    int *paramIndex;
+    
+    paramIndex = (int *)*functionPvt;
+    
+    if (paramIndex == 0) {
+        paramIndex = (int *)malloc(sizeof(int));
+        // This is the first time we have been called for this attribute.
+        // Convert paramString to paramIndex
+        
+        if (!strcmp(paramString, "PI")) {
+            *paramIndex = functPi;
+        }
+        else if (!strcmp(paramString, "E")) {
+            *paramIndex = functE;
+        } 
+        else if (!strcmp(paramString, "10")) {
+            *paramIndex = functTen;
+        }
+        else if (!strcmp(paramString, "GETTYSBURG")) {
+            *paramIndex = functGettysburg;
+        } 
+        else {
+            printf("Error, unknown parameter string = %s\n", paramString);
+            *paramIndex = functPi;
+            return ND_ERROR;
+        }
+        *functionPvt = paramIndex;
     }
-    else if (!strcmp(paramString, "E")) {
-        pAttribute->setValue(NDAttrFloat64, &_E);
-    } 
-    else if (!strcmp(paramString, "10")) {
-        pAttribute->setValue(NDAttrInt32, &ten);
-    }
-    else if (!strcmp(paramString, "GETTYSBURG")) {
-        pAttribute->setValue(NDAttrString, (char *)gettysburg);
-    } 
-    else {
-        return ND_ERROR;
+    
+    switch (*paramIndex) {
+        case functPi:
+            pAttribute->setValue(NDAttrFloat64, &_PI);
+            break;
+        
+        case functE:
+            pAttribute->setValue(NDAttrFloat64, &_E);
+            break;
+        
+        case functTen:
+            pAttribute->setValue(NDAttrInt32, &ten);
+            break;
+            
+        case functGettysburg:
+            pAttribute->setValue(NDAttrString, (char *)gettysburg);
+            break;
+            
+        default:
+            return ND_ERROR;
     }
     return ND_SUCCESS;
 }
