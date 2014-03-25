ce43410ce36870912fec036f7aaeeffe43597ff0
mp49 <mp49@users.noreply.github.com>
2014-03-25 10:55:35 -0400

    Remove LGPLv3 license.

--- ADApp/pluginSrc/NDPluginAttribute.cpp
+++ ADApp/pluginSrc/NDPluginAttribute.cpp
@@ -1,21 +1,3 @@
-/**
- * Author: Diamond Light Source, Copyright 2013
- *
- * License: This file is part of 'spectraPlugins'
- * 
- * 'spectraPlugins' is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * 'spectraPlugins' is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public License
- * along with 'spectraPlugins'.  If not, see <http://www.gnu.org/licenses/>.
- */
 
 /*
  * NDPluginAttribute.cpp
@@ -23,7 +5,7 @@
  * Extract an Attribute from an NDArray and publish the value in an array.
  * Author: Matthew Pearson
  *
- * Dec 2012
+ * March 2014.
  */
 
 #include <stdlib.h>
