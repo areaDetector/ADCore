59088c3dadbd64c6768a558c464f6ce177fab8e0
Mark Rivers <rivers@cars.uchicago.edu>
2014-05-17 09:33:16 -0500

    Replaced tabs

--- ADApp/ADSrc/parseAreaPrefixes.c
+++ ADApp/ADSrc/parseAreaPrefixes.c
@@ -17,56 +17,56 @@
 #include "epicsExport.h"
 
 void parseAreaPrefixes( char *waveformName) {
-	int status;
-	const char *functionName = "parseAreaPrefixes";
-	char inBuffer[128];
-	char outBuffer[2048];
-	char waveformNameFull[128];
-	char *indexPtr;
-	int nFound;
-	size_t foundLen;
-	DBADDR dbaddr;
-	DBADDR *pdbaddr = &dbaddr;
-	DBENTRY dbentry;
-	DBENTRY *pdbentry = &dbentry;
-	if (!pdbbase) {
-		printf("%s: No database loaded\n", functionName);
-		return;
-	}
-	status = 0;
-	nFound = 0;
-	outBuffer[0] = '\0';
-	dbInitEntry(pdbbase, pdbentry);
-	status = dbFindRecordType(pdbentry, "asyn");
-	while (!status) {
-		status=dbFirstRecord(pdbentry);
-		while (!status) {
-			strcpy(inBuffer, dbGetRecordName(pdbentry));
-			status = dbFindInfo(pdbentry, "ADType");
-			if (!status) {
-				if (nFound != 0){
-					strcat( outBuffer, " ");
-				}
-				indexPtr = strstr(inBuffer, "AsynIO");
-				foundLen = indexPtr - inBuffer;
-				strncat( outBuffer, inBuffer, foundLen);
-				nFound++;
-			}
-			status = dbNextRecord(pdbentry);
-		}
-	}
-	dbFinishEntry(pdbentry);
+    int status;
+    const char *functionName = "parseAreaPrefixes";
+    char inBuffer[128];
+    char outBuffer[2048];
+    char waveformNameFull[128];
+    char *indexPtr;
+    int nFound;
+    size_t foundLen;
+    DBADDR dbaddr;
+    DBADDR *pdbaddr = &dbaddr;
+    DBENTRY dbentry;
+    DBENTRY *pdbentry = &dbentry;
+    if (!pdbbase) {
+        printf("%s: No database loaded\n", functionName);
+        return;
+    }
+    status = 0;
+    nFound = 0;
+    outBuffer[0] = '\0';
+    dbInitEntry(pdbbase, pdbentry);
+    status = dbFindRecordType(pdbentry, "asyn");
+    while (!status) {
+        status=dbFirstRecord(pdbentry);
+        while (!status) {
+            strcpy(inBuffer, dbGetRecordName(pdbentry));
+            status = dbFindInfo(pdbentry, "ADType");
+            if (!status) {
+                if (nFound != 0){
+                    strcat( outBuffer, " ");
+                }
+                indexPtr = strstr(inBuffer, "AsynIO");
+                foundLen = indexPtr - inBuffer;
+                strncat( outBuffer, inBuffer, foundLen);
+                nFound++;
+            }
+            status = dbNextRecord(pdbentry);
+        }
+    }
+    dbFinishEntry(pdbentry);
 
-	/** get the channel for the waveform */
+    /** get the channel for the waveform */
 
-	sprintf ( waveformNameFull, "%s", waveformName);
-	status = dbNameToAddr( waveformName, pdbaddr);
-	if ( status == 0 ) {
-		dbPutField(pdbaddr, DBR_UCHAR, outBuffer, (long)strlen(outBuffer));
-	}
-	else {
-		printf("Cannot find channel %s\n", waveformName);
-	}
+    sprintf ( waveformNameFull, "%s", waveformName);
+    status = dbNameToAddr( waveformName, pdbaddr);
+    if ( status == 0 ) {
+        dbPutField(pdbaddr, DBR_UCHAR, outBuffer, (long)strlen(outBuffer));
+    }
+    else {
+        printf("Cannot find channel %s\n", waveformName);
+    }
 
 }
 
@@ -77,12 +77,12 @@ static const iocshFuncDef parseFuncDef = {"parseAreaPrefixes", 1, parseArgs};
 
 /* Wrapper called by iocsh, select the argumentTypes that parseAreaFixes needs */
 static void parseCallFunc(const iocshArgBuf * args) {
-	parseAreaPrefixes( args[0].sval);
+    parseAreaPrefixes( args[0].sval);
 }
 
 /*Registration routine, runs at startup */
 static void parseRegister(void){
-	iocshRegister(&parseFuncDef, parseCallFunc);
+    iocshRegister(&parseFuncDef, parseCallFunc);
 }
 
 epicsExportRegistrar(parseRegister);
