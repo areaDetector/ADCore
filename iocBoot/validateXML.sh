b9616f2ccfcc552db8358c3488bea86d29d960ae
Mark Rivers <rivers@cars.uchicago.edu>
2014-10-14 17:45:25 -0500

    Added the HDF5 validation; fixed typo

--- iocBoot/validateXML.sh
+++ iocBoot/validateXML.sh
@@ -11,14 +11,17 @@
 # Validate the XML Attribute and Template files
 
 ATTRIBUTE_SCHEMA="--noout --schema ./NDAttributes.xsd"
-#TEMPLATE_SCHEMA="--noout --schema ./NeXus_templates.xsd"
-TEMPLATE_SCHEMA="--noout --schematron ./template.sch"
+NEXUS_TEMPLATE_SCHEMA="--noout --schematron ./template.sch"
+HDF5_TEMPLATE_SCHEMA="--noout --schema ./hdf5_xml_layout_schema.xsd"
 
 # NDArray Attribute declaration files
-mllint ${ATTRIBUTE_SCHEMA} ../iocs/simDetectorIOC/iocBoot/iocSimDetector/ROIAttributes.xml
+xmllint ${ATTRIBUTE_SCHEMA} ../iocs/simDetectorIOC/iocBoot/iocSimDetector/ROIAttributes.xml
 xmllint ${ATTRIBUTE_SCHEMA} ../iocs/simDetectorIOC/iocBoot/iocSimDetector/netCDFAttributes.xml
 xmllint ${ATTRIBUTE_SCHEMA} ../iocs/simDetectorIOC/iocBoot/iocSimDetector/simDetectorAttributes.xml
 
 # NeXus file writer plugin template files
-#
-xmllint ${TEMPLATE_SCHEMA} ../iocs/simDetectorIOC/iocBoot/iocSimDetector/NexusTemplate.xml
+xmllint ${NEXUS_TEMPLATE_SCHEMA} ../iocs/simDetectorIOC/iocBoot/iocSimDetector/NexusTemplate.xml
+
+# HDF5 file writer plugin template files
+xmllint ${HDF5_TEMPLATE_SCHEMA} ../iocs/simDetectorIOC/iocBoot/iocSimDetector/hdf5_layout_demo.xml
+xmllint ${HDF5_TEMPLATE_SCHEMA} ../iocs/simDetectorIOC/iocBoot/iocSimDetector/hdf5_layout_nexus.xml
