23b2ebb0efe52e46099d112d70fb8b25bbca6255
Mark Rivers <rivers@cars.uchicago.edu>
2014-10-14 17:34:00 -0500

    Added comment about EPICS_CA_MAX_ARRAY_BYTES

--- iocs/simDetectorIOC/iocBoot/iocSimDetector/st.cmd
+++ iocs/simDetectorIOC/iocBoot/iocSimDetector/st.cmd
@@ -12,6 +12,18 @@ epicsEnvSet("XSIZE",  "1024")
 epicsEnvSet("YSIZE",  "1024")
 epicsEnvSet("NCHANS", "2048")
 
+# The EPICS environment variable EPICS_CA_MAX_ARRAY_BYTES needs to be set to a value at least as large
+# as the largest image that the standard arrays plugin will send.
+# That vlaue is $(XSIZE) * $(YSIZE) * sizeof(FTVL data type) for the FTVL used when loading the NDStdArrays.template file.
+# The variable can be set in the environment before running the IOC or it can be set here.
+# It is often convenient to set it in the environment outside the IOC to the largest array any client 
+# or server will need.  For example 10000000 (ten million) bytes may be enough.
+# If it is set here then remember to also set it outside the IOC for any CA clients that need to access the waveform record.  
+# Do not set EPICS_CA_MAX_ARRAY_BYTES to a value much larger than that required, because EPICS Channel Access actually
+# allocates arrays of this size every time it needs a buffer larger than 16K.
+# Uncomment the following line to set it in the IOC.
+#epicsEnvSet("EPICS_CA_MAX_ARRAY_BYTES", "10000000")
+
 # Create a simDetector driver
 # simDetectorConfig(const char *portName, int maxSizeX, int maxSizeY, int dataType,
 #                   int maxBuffers, int maxMemory, int priority, int stackSize)
