0694f71e40f13a058223f17dccd2ae9a808e4222
Mark Rivers <rivers@cars.uchicago.edu>
2015-03-03 11:45:52 -0600

    Fixed typo

--- ADApp/pluginSrc/NDPluginCircularBuff.h
+++ ADApp/pluginSrc/NDPluginCircularBuff.h
@@ -21,7 +21,7 @@ typedef struct NDCircularBuff {
 #define NDPluginCircularBuffTriggerAValString        "CIRCULAR_BUFF_TRIGGER_A_VAL" /* (asynFloat64,      r/o) Trigger A value */
 #define NDPluginCircularBuffTriggerBValString        "CIRCULAR_BUFF_TRIGGER_B_VAL" /* (asynFloat64,      r/o) Trigger B value */
 #define NDPluginCircularBuffTriggerCalcString        "CIRCULAR_BUFF_TRIGGER_CALC"  /* (asynOctetWrite,   r/w) Trigger calculation expression */
-#define NDPluginCircularBuffTriggerCalcValString     "CIRCULAR_BUFF_TRIGGER_VAL"   /* (asynFloat64,      r/o) Trigger calculation value */
+#define NDPluginCircularBuffTriggerCalcValString     "CIRCULAR_BUFF_TRIGGER_CALC_VAL" /* (asynFloat64,   r/o) Trigger calculation value */
 #define NDPluginCircularBuffPreTriggerString         "CIRCULAR_BUFF_PRE_TRIGGER"   /* (asynInt32,        r/w) Number of pre-trigger images */
 #define NDPluginCircularBuffPostTriggerString        "CIRCULAR_BUFF_POST_TRIGGER"  /* (asynInt32,        r/w) Number of post-trigger images */
 #define NDPluginCircularBuffCurrentImageString       "CIRCULAR_BUFF_CURRENT_IMAGE" /* (asynInt32,        r/o) Number of the current image */
