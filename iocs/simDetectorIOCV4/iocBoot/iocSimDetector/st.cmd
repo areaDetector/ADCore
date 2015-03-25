308f296459d1c5334810ad8bd3819a8c3856daf9
Bruno Martins <bmartins@bnl.gov>
2015-03-25 13:36:39 -0400

    Fix db path on V4 simulator IOC. Fix start_epics permissions

--- iocs/simDetectorIOCV4/iocBoot/iocSimDetector/st.cmd
+++ iocs/simDetectorIOCV4/iocBoot/iocSimDetector/st.cmd
@@ -14,25 +14,26 @@ epicsEnvSet("NCHANS", "2048")
 
 #epicsEnvSet("EPICS_CA_MAX_ARRAY_BYTES", "10000000")
 epicsEnvSet("EPICS_CA_MAX_ARRAY_BYTES", "4000000")
+epicsEnvSet("EPICS_DB_INCLUDE_PATH", "$(ADCORE)/db")
 
 # Create a simDetector driver
 # simDetectorConfig(const char *portName, int maxSizeX, int maxSizeY, int dataType,
 #                   int maxBuffers, int maxMemory, int priority, int stackSize)
 simDetectorConfig("$(PORT)", $(XSIZE), $(YSIZE), 1, 0, 0)
-dbLoadRecords("$(ADCORE)/db/ADBase.template",     "P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
-dbLoadRecords("$(ADCORE)/db/simDetector.template","P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
+dbLoadRecords("ADBase.template",     "P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
+dbLoadRecords("simDetector.template","P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
 
 # Create a V4 server
 NDV4ServerConfigure("V4", 3, 0, "$(PORT)", 0, "testMP")
-dbLoadRecords("$(ADCORE)/db/NDPluginBase.template","P=$(PREFIX),R=V4:,PORT=V4,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT),NDARRAY_ADDR=0")
+dbLoadRecords("NDPluginBase.template","P=$(PREFIX),R=V4:,PORT=V4,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT),NDARRAY_ADDR=0")
 
 # Create a standard arrays plugin, set it to get data from first simDetector driver.
 NDStdArraysConfigure("Image1", 3, 0, "$(PORT)", 0)
-dbLoadRecords("$(ADCORE)/db/NDPluginBase.template","P=$(PREFIX),R=image1:,PORT=Image1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT),NDARRAY_ADDR=0")
+dbLoadRecords("NDPluginBase.template","P=$(PREFIX),R=image1:,PORT=Image1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT),NDARRAY_ADDR=0")
 
 # This creates a waveform large enough for 1024x1024x1 (Mono)
 # This waveform only allows transporting 8-bit images
-dbLoadRecords("$(ADCORE)/db/NDStdArrays.template", "P=$(PREFIX),R=image1:,PORT=Image1,ADDR=0,TIMEOUT=1,TYPE=Int8,FTVL=UCHAR,NELEMENTS=1048576")
+dbLoadRecords("NDStdArrays.template", "P=$(PREFIX),R=image1:,PORT=Image1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT),TYPE=Int8,FTVL=UCHAR,NELEMENTS=1048576")
 
 iocInit()
 
