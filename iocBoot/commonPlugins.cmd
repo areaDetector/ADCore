6633f6c1d9094e1ad0e4373ac65d3a04db19317b
Mark Rivers <rivers@cars.uchicago.edu>
2015-07-22 18:32:03 -0500

    Added comments on dbLoadRecords for scan.db, yySseq.db, iocAdminSoft.db, and alive.db saying they are optional

--- iocBoot/commonPlugins.cmd
+++ iocBoot/commonPlugins.cmd
@@ -98,14 +98,17 @@ dbLoadRecords("NDAttributeN.template", "P=$(PREFIX),R=Attr1:6:,  PORT=ATTR1,ADDR
 dbLoadRecords("NDAttributeN.template", "P=$(PREFIX),R=Attr1:7:,  PORT=ATTR1,ADDR=6,TIMEOUT=1,NCHANS=$(NCHANS)")
 dbLoadRecords("NDAttributeN.template", "P=$(PREFIX),R=Attr1:8:,  PORT=ATTR1,ADDR=7,TIMEOUT=1,NCHANS=$(NCHANS)")
 
-# Load scan records
+# Optional: load scan records
 dbLoadRecords("$(SSCAN)/sscanApp/Db/scan.db", "P=$(PREFIX),MAXPTS1=2000,MAXPTS2=200,MAXPTS3=20,MAXPTS4=10,MAXPTSH=10")
 
-# Load sseq record for acquisition sequence
+# Optional: load sseq record for acquisition sequence
 dbLoadRecords("$(CALC)/calcApp/Db/yySseq.db", "P=$(PREFIX), S=AcquireSequence")
 
-# Load devIocStats records
+# Optional: load devIocStats records (requires DEVIOCSTATS module)
 dbLoadRecords("$(DEVIOCSTATS)/db/iocAdminSoft.db", "IOC=$(PREFIX)")
+
+# Optional: load alive record (requires ALIVE module)
+#dbLoadRecords("$(ALIVE)/aliveApp/Db/alive.db", "P=$(PREFIX),RHOST=192.168.1.254")
 
 set_requestfile_path("./")
 set_requestfile_path("$(ADCORE)/ADApp/Db")
