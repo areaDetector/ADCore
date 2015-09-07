8c9982c9f26cabd755f782eb77cbda5d2d870622
Ulrik Kofoed Pedersen <ulrik.pedersen@diamond.ac.uk>
2015-09-07 13:36:55 +0100

    travis config: removed all synApps deps bar asyn
    This is possible as ADCore is now only a library and contain no IOCs

--- ci/configure.sh
+++ ci/configure.sh
@@ -16,18 +16,4 @@ echo "USR_CXXFLAGS_Linux=--coverage"         >> configure/CONFIG_SITE.linux-x86_
 echo "USR_LDFLAGS_Linux=--coverage"          >> configure/CONFIG_SITE.linux-x86_64.Common 
 
 echo "ASYN=`pwd`/external/asyn4-26"          >> configure/RELEASE.local
-echo "BUSY=`pwd`/external/busy-1-6-1"        >> configure/RELEASE.local
-echo "CALC=`pwd`/external/calc-3-4-2"        >> configure/RELEASE.local
-echo "SSCAN=`pwd`/external/sscan-2-10"       >> configure/RELEASE.local
-echo "AUTOSAVE=`pwd`/external/autosave-5-5"  >> configure/RELEASE.local
-echo "DEVIOCSTATS=`pwd`/external/iocStats-3.1.14"  >> configure/RELEASE.local
-
-# Configure the example IOCs in preparation for build
-echo "BUILD_IOCS=YES"                        >> configure/CONFIG_SITE.linux-x86_64.Common
-cp configure/RELEASE.local                      iocs/simDetectorIOC/configure/
-cp configure/CONFIG_SITE.linux-x86_64.Common    iocs/simDetectorIOC/configure/
-echo "ADCORE=`pwd`"                          >> iocs/simDetectorIOC/configure/RELEASE.local
-cp configure/RELEASE.local                      iocs/simDetectorNoIOC/configure/
-cp configure/CONFIG_SITE.linux-x86_64.Common    iocs/simDetectorNoIOC/configure/
-echo "ADCORE=`pwd`"                          >> iocs/simDetectorNoIOC/configure/RELEASE.local
 
