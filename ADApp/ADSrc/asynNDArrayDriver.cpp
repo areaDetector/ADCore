83b84261cf4c992b89778ed072e58f96e75e5024
Mark Rivers <rivers@cars.uchicago.edu>
2016-02-28 21:39:29 -0600

    Added asynDrvUserMask to interfaceMask;  this should always be set

--- ADApp/ADSrc/asynNDArrayDriver.cpp
+++ ADApp/ADSrc/asynNDArrayDriver.cpp
@@ -602,7 +602,7 @@ asynNDArrayDriver::asynNDArrayDriver(const char *portName, int maxAddr, int numP
                                      size_t maxMemory, int interfaceMask, int interruptMask,
                                      int asynFlags, int autoConnect, int priority, int stackSize)
     : asynPortDriver(portName, maxAddr, numParams+NUM_NDARRAY_PARAMS, 
-                     interfaceMask | asynInt32Mask | asynFloat64Mask | asynOctetMask | asynInt32ArrayMask | asynGenericPointerMask, 
+                     interfaceMask | asynInt32Mask | asynFloat64Mask | asynOctetMask | asynInt32ArrayMask | asynGenericPointerMask | asynDrvUserMask, 
                      interruptMask | asynInt32Mask | asynFloat64Mask | asynOctetMask | asynInt32ArrayMask | asynGenericPointerMask,
                      asynFlags, autoConnect, priority, stackSize),
       pNDArrayPool(NULL)
