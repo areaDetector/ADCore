aceee9304f5b5a7be6810cda1c8b4ee24f9f8c00
Mark Rivers <rivers@cars.uchicago.edu>
2016-03-12 12:40:08 -0600

    Removed FFT related items, now in NDPluginFFT

--- ADApp/pluginSrc/NDPluginTimeSeries.cpp
+++ ADApp/pluginSrc/NDPluginTimeSeries.cpp
@@ -2,7 +2,6 @@
  * NDPluginTimeSeries.cpp
  *
  * Plugin that creates time-series arrays from callback data.
- * Optionally computes the FFT of the time-series data. 
  * 
  * @author Mark Rivers 
  * @date February 2016
@@ -14,21 +13,23 @@
 #include <math.h>
 
 #include <epicsString.h>
-#include <epicsMutex.h>
 #include <epicsTime.h>
 #include <iocsh.h>
 
 #include <asynDriver.h>
-#include <asynFloat64SyncIO.h>
 
 #include <NDArray.h>
 
 #include <epicsExport.h>
 
 #include "NDPluginTimeSeries.h"
-#include "fft_1d.h"
 
 #define DEFAULT_NUM_TSPOINTS 2048
+
+enum {
+  TSAcquireModeFixed,
+  TSAcquireModeCircular
+};
 
 /** Constructor for NDPluginTimeSeries; most parameters are simply passed to NDPluginDriver::NDPluginDriver.
   * \param[in] portName The name of the asyn port driver to be created.
@@ -58,9 +59,7 @@ NDPluginTimeSeries::NDPluginTimeSeries(const char *portName, int queueSize, int 
              asynFloat64Mask | asynFloat64ArrayMask | asynGenericPointerMask,
              asynFloat64Mask | asynFloat64ArrayMask | asynGenericPointerMask,
              ASYN_MULTIDEVICE, 1, priority, stackSize),
-    numAverage_(1), timePerPoint_(0),
-    timeAxis_(0), freqAxis_(0), timeStamp_(0), 
-    timeSeries_(0), timeCircular_(0), FFTReal_(0), FFTImaginary_(0), FFTAbsValue_(0)
+    numAverage_(1), timePerPoint_(0), timeAxis_(0), timeStamp_(0), timeSeries_(0), timeCircular_(0)
 {
   //const char *functionName = "NDPluginTimeSeries::NDPluginTimeSeries";
 
@@ -80,18 +79,13 @@ NDPluginTimeSeries::NDPluginTimeSeries(const char *portName, int queueSize, int 
   createParam(TSNumAverageString,              asynParamInt32, &P_TSNumAverage);
   createParam(TSElapsedTimeString,           asynParamFloat64, &P_TSElapsedTime);
   createParam(TSAcquireModeString,             asynParamInt32, &P_TSAcquireMode);
-  createParam(TSComputeFFTString,              asynParamInt32, &P_TSComputeFFT);
   createParam(TSTimeAxisString,         asynParamFloat64Array, &P_TSTimeAxis);
-  createParam(TSFreqAxisString,         asynParamFloat64Array, &P_TSFreqAxis);
   createParam(TSTimestampString,        asynParamFloat64Array, &P_TSTimestamp);
   
   /* Per-signal parameters */
   createParam(TSSignalNameString,              asynParamOctet, &P_TSSignalName);
   createParam(TSSignalUseString,               asynParamInt32, &P_TSSignalUse);
   createParam(TSTimeSeriesString,       asynParamFloat64Array, &P_TSTimeSeries);
-  createParam(TSFFTRealString,          asynParamFloat64Array, &P_TSFFTReal);
-  createParam(TSFFTImaginaryString,     asynParamFloat64Array, &P_TSFFTImaginary);
-  createParam(TSFFTAbsValueString,      asynParamFloat64Array, &P_TSFFTAbsValue);
  
   /* Set the plugin type string */
   setStringParam(NDPluginDriverPluginType, "NDPluginTimeSeries");
@@ -110,7 +104,6 @@ NDPluginTimeSeries::NDPluginTimeSeries(const char *portName, int queueSize, int 
 void NDPluginTimeSeries::computeNumAverage()
 {
   if (timePerPoint_ == 0) {
-    // The driver must not support getting the time per point on the asynFloat64 interface
     numAverage_ = 1;
     averagingTimeActual_ = averagingTimeRequested_;
   }
@@ -136,83 +129,37 @@ void NDPluginTimeSeries::allocateArrays()
   if (timeStamp_)     free(timeStamp_);
   if (timeSeries_)    free(timeSeries_);
   if (timeCircular_)  free(timeCircular_);
-  if (FFTReal_)       free(FFTReal_);
-  if (FFTImaginary_)  free(FFTImaginary_);
-  if (FFTAbsValue_)   free(FFTAbsValue_);
-
-  numFreqPoints_ = numTimePoints_ / 2;
 
   timeStamp_    = (double *)calloc(maxSignals_*numTimePoints_, sizeof(double));
   timeSeries_   = (double *)calloc(maxSignals_*numTimePoints_, sizeof(double));
   timeCircular_ = (double *)calloc(maxSignals_*numTimePoints_, sizeof(double));
-  FFTComplex_   = (double *)calloc(maxSignals_*numFreqPoints_, sizeof(double)*2);
-  FFTReal_      = (double *)calloc(maxSignals_*numFreqPoints_, sizeof(double));
-  FFTImaginary_ = (double *)calloc(maxSignals_*numFreqPoints_, sizeof(double));
-  FFTAbsValue_  = (double *)calloc(maxSignals_*numFreqPoints_, sizeof(double));
   createAxisArrays();
 }
 
 void NDPluginTimeSeries::zeroArrays()
 {
   size_t timeSize = maxSignals_ * numTimePoints_ * sizeof(double);
-  size_t freqSize = maxSignals_ * numFreqPoints_ * sizeof(double);
 
   memset(timeStamp_,    0, numTimePoints_*sizeof(double));
   memset(timeSeries_,   0, timeSize);
   memset(timeCircular_, 0, timeSize);
-  memset(FFTComplex_,   0, freqSize * 2); // Complex data
-  memset(FFTReal_,      0, freqSize);
-  memset(FFTImaginary_, 0, freqSize);
-  memset(FFTAbsValue_,  0, freqSize);
 }
-
-void NDPluginTimeSeries::computeFFTs()
-{
-  int i, j;
-  
-  /* Compute the FFTs of each array */
-  for (i=0; i<maxSignals_; i++) {
-    for (j=0; j<numTimePoints_; j++) {
-      FFTComplex_[2*j] = timeSeries_[i*numTimePoints_ + j];
-      FFTComplex_[2*j+1] = 0.;
-    }
-    fft_1d(FFTComplex_-1, numTimePoints_, 1);
-    /* Start at 1 so we don't copy DC offset which messes up scaling of plots */
-    for (j=1; j<numFreqPoints_; j++) {
-      FFTReal_     [i*numFreqPoints_ + j] = FFTComplex_[2*j]; 
-      FFTImaginary_[i*numFreqPoints_ + j] = FFTComplex_[2*j+1]; 
-      FFTAbsValue_ [i*numFreqPoints_ + j] = sqrt((FFTComplex_[2*j]   * FFTComplex_[2*j] + 
-                                                  FFTComplex_[2*j+1] * FFTComplex_[2*j+1])
-                                                 / (numTimePoints_ * numTimePoints_));
-    }
-  }
-}         
 
 void NDPluginTimeSeries::createAxisArrays()
 {
-  double freqStep;
   int i;
   
   if (timeAxis_) free(timeAxis_);
-  if (freqAxis_) free(freqAxis_);
   timeAxis_ = (double *)calloc(numTimePoints_, sizeof(double));
-  freqAxis_ = (double *)calloc(numFreqPoints_, sizeof(double));
   for (i=0; i<numTimePoints_; i++) {
     timeAxis_[i] = i*averagingTimeActual_;
   }
-  // Check this - are the frequencies correct, or off-by-one?
-  freqStep = 0.5 / averagingTimeActual_ / (numFreqPoints_ - 2);
-  for (i=0; i<numFreqPoints_; i++) {
-    freqAxis_[i] = i * freqStep;
-  }
   doCallbacksFloat64Array(timeAxis_, numTimePoints_, P_TSTimeAxis, 0);
-  doCallbacksFloat64Array(freqAxis_, numFreqPoints_, P_TSFreqAxis, 0);
 }
 
 /**
- * Templated function to calculate statistics on different NDArray data types.
+ * Templated function to append to time series on different NDArray data types.
  * \param[in] NDArray The pointer to the NDArray object
- * \param[in] NDROI The pointer to the NDROI object
  * \return asynStatus
  */
 template <typename epicsType>
@@ -452,22 +399,16 @@ void NDPluginTimeSeries::doTimeSeriesCallbacks()
   int signal;
   size_t numCopy;
   double *src, *dst;
-  int computeFFT;
-  
-  getIntegerParam(P_TSComputeFFT, &computeFFT);
-
   // If we are in circular buffer mode then copy data from timeCircular_ to timeSeries_
   if (acquireMode_ == TSAcquireModeCircular) {
     for (signal=0; signal<maxSignals_; signal++) {
       numCopy = numTimePoints_ - currentTimePoint_;
       src = timeCircular_ + (signal * numTimePoints_) + currentTimePoint_;
       dst = timeSeries_   + (signal * numTimePoints_);
-//printf("memcpy #1, src=%p, dst=%p, num=%d\n", src, dst, (int)(numCopy*sizeof(double)));
       memcpy(dst, src, numCopy*sizeof(double));
       numCopy = currentTimePoint_;
       src = timeCircular_ + (signal * numTimePoints_);
       dst = timeSeries_   + (signal * numTimePoints_) + numTimePoints_ - currentTimePoint_;
-//printf("memcpy #1, src=%p, dst=%p, num=%d\n", src, dst, (int)(numCopy*sizeof(double)));
       memcpy(dst, src, numCopy*sizeof(double));
       doCallbacksFloat64Array(timeSeries_ + signal*numTimePoints_, numTimePoints_, P_TSTimeSeries, signal);
     }
@@ -476,16 +417,6 @@ void NDPluginTimeSeries::doTimeSeriesCallbacks()
     /* Do time series array callbacks */
     for (signal=0; signal<maxSignals_; signal++) {
       doCallbacksFloat64Array(timeSeries_ + signal*numTimePoints_, currentTimePoint_, P_TSTimeSeries, signal);
-    }
-  }
-  
-  if (computeFFT) {
-    computeFFTs();
-    /* Do FFT array callbacks */
-    for (signal=0; signal<maxSignals_; signal++) {
-      doCallbacksFloat64Array(FFTReal_      + signal*numFreqPoints_, numFreqPoints_, P_TSFFTReal,      signal);
-      doCallbacksFloat64Array(FFTImaginary_ + signal*numFreqPoints_, numFreqPoints_, P_TSFFTImaginary, signal);
-      doCallbacksFloat64Array(FFTAbsValue_  + signal*numFreqPoints_, numFreqPoints_, P_TSFFTAbsValue,  signal);
     }
   }
 }
