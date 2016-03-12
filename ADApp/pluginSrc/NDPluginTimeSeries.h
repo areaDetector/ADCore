175429bd21cda126f00048df5f21cf4ebb642aeb
Mark Rivers <rivers@cars.uchicago.edu>
2016-03-12 12:39:51 -0600

    Removed FFT related items, now in NDPluginFFT

--- ADApp/pluginSrc/NDPluginTimeSeries.h
+++ ADApp/pluginSrc/NDPluginTimeSeries.h
@@ -2,7 +2,6 @@
  * NDPluginTimeSeries.h
  *
  * Plugin that creates time-series arrays from callback data.
- * Optionally computes the FFT of the time-series data. 
  * 
  * @author Mark Rivers 
  * @date February 2016
@@ -27,34 +26,16 @@
 #define TSNumAverageString      "TS_NUM_AVERAGE"      /* (asynInt32,        r/o) Time points to average */
 #define TSElapsedTimeString     "TS_ELAPSED_TIME"     /* (asynFloat64,      r/o) Elapsed acquisition time */
 #define TSAcquireModeString     "TS_ACQUIRE_MODE"     /* (asynInt32,        r/w) Acquire mode */
-#define TSComputeFFTString      "TS_COMPUTE_FFT"      /* (asynInt32,        r/w) Compute FFTs */
 #define TSTimeAxisString        "TS_TIME_AXIS"        /* (asynFloat64Array, r/o) Time axis array */
-#define TSFreqAxisString        "TS_FREQ_AXIS"        /* (asynFloat64Array, r/o) Frequency axis array */
 #define TSTimestampString       "TS_TIMESTAMP"        /* (asynFloat64Array, r/o) Series of timestamps */
 
 /* Per-signal parameters */
 #define TSSignalNameString      "TS_SIGNAL_NAME"      /* (asynOctet,        r/w) Name of this signal */
 #define TSSignalUseString       "TS_SIGNAL_USE"       /* (asynInt32,        r/w) Use this signal? */
 #define TSTimeSeriesString      "TS_TIME_SERIES"      /* (asynFloat64Array, r/o) Time series array */
-#define TSFFTRealString         "TS_FFT_REAL"         /* (asynFloat64Array, r/o) Real part of FFT */
-#define TSFFTImaginaryString    "TS_FFT_IMAGINARY"    /* (asynFloat64Array, r/o) Imaginary part of FFT */
-#define TSFFTAbsValueString     "TS_FFT_ABS_VALUE"    /* (asynFloat64Array, r/o) Absolute value of FFT */
-
-enum {
-  TSEraseStart,
-  TSStart,
-  TSStop,
-  TSRead
-};
-
-enum {
-  TSAcquireModeFixed,
-  TSAcquireModeCircular
-};
 
 
-
-/** Compute time series and FFTs on signals */
+/** Compute time series on signals */
 class epicsShareClass NDPluginTimeSeries : public NDPluginDriver {
 public:
   NDPluginTimeSeries(const char *portName, int queueSize, int blockingCallbacks, 
@@ -80,19 +61,14 @@ protected:
   int P_TSNumAverage;
   int P_TSElapsedTime;
   int P_TSAcquireMode;
-  int P_TSComputeFFT;
   int P_TSTimeAxis;
-  int P_TSFreqAxis;
   int P_TSTimestamp;
 
   // Per-signal parameters
   int P_TSSignalName;
   int P_TSSignalUse;
   int P_TSTimeSeries;
-  int P_TSFFTReal;
-  int P_TSFFTImaginary;
-  int P_TSFFTAbsValue;
-  #define LAST_NDPLUGIN_TIME_SERIES_PARAM P_TSFFTAbsValue
+  #define LAST_NDPLUGIN_TIME_SERIES_PARAM P_TSTimeSeries
                                 
 private:
   template <typename epicsType> asynStatus doAddToTimeSeriesT(NDArray *pArray);
@@ -102,14 +78,10 @@ private:
   void allocateArrays();
   void zeroArrays();
   void createAxisArrays();
-  void computeFFTs();
   void computeNumAverage();
 
-  asynUser *pasynUserInput_;
-  asynUser *pasynUserFloat64SyncIO_;
   int maxSignals_;
   int numTimePoints_;
-  int numFreqPoints_;
   int currentTimePoint_;
   int numAverage_;
   int numAveraged_;
@@ -120,14 +92,9 @@ private:
   epicsTimeStamp startTime_;
   double *averageStore_;
   double *timeAxis_;
-  double *freqAxis_;
   double *timeStamp_;
   double *timeSeries_;
   double *timeCircular_;
-  double *FFTComplex_;
-  double *FFTReal_;
-  double *FFTImaginary_;
-  double *FFTAbsValue_;
 };
 
 #define NUM_NDPLUGIN_TIME_SERIES_PARAMS (int)(&LAST_NDPLUGIN_TIME_SERIES_PARAM - &FIRST_NDPLUGIN_TIME_SERIES_PARAM + 1)
