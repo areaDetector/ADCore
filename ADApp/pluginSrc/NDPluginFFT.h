7bd7f5ba70d183d1dda1b0244990e951ad3e27b8
Mark Rivers <rivers@cars.uchicago.edu>
2016-03-13 15:30:26 -0500

    Add TimeAxis, TimeSeries

--- ADApp/pluginSrc/NDPluginFFT.h
+++ ADApp/pluginSrc/NDPluginFFT.h
@@ -17,10 +17,12 @@
 
 /* Per-plugin parameters */
 #define FFTReadString            "FFT_READ"             /* (asynInt32,        r/w) Read data */
+#define FFTTimeAxisString        "FFT_TIME_AXIS"        /* (asynFloat64Array, r/o) Time axis array */
 #define FFTFreqAxisString        "FFT_FREQ_AXIS"        /* (asynFloat64Array, r/o) Frequency axis array */
 #define FFTTimePerPointString    "FFT_TIME_PER_POINT"   /* (asynFloat64,      r/o) Time per time point from driver */
 
 /* Per-signal parameters */
+#define FFTTimeSeriesString      "FFT_TIME_SERIES"      /* (asynFloat64Array, r/o) Time series data */
 #define FFTRealString            "FFT_REAL"             /* (asynFloat64Array, r/o) Real part of FFT */
 #define FFTImaginaryString       "FFT_IMAGINARY"        /* (asynFloat64Array, r/o) Imaginary part of FFT */
 #define FFTAbsValueString        "FFT_ABS_VALUE"        /* (asynFloat64Array, r/o) Absolute value of FFT */
@@ -42,9 +44,11 @@ protected:
   int P_FFTRead;
   #define FIRST_NDPLUGIN_FFT_PARAM P_FFTRead
   int P_FFTTimePerPoint;
+  int P_FFTTimeAxis;
   int P_FFTFreqAxis;
 
   // Per-signal parameters
+  int P_FFTTimeSeries;
   int P_FFTReal;
   int P_FFTImaginary;
   int P_FFTAbsValue;
@@ -59,9 +63,11 @@ private:
   void computeFFT_1D();
 
   int maxSignals_;
+  int numSignals_;
   int numTimePoints_;
   int numFreqPoints_;
   double timePerPoint_; /* Actual time between points in input arrays */
+  double *timeAxis_;
   double *freqAxis_;
   double *timeSeries_;
   double *FFTComplex_;
