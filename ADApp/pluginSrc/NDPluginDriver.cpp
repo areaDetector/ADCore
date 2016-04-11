e7f56e1fd3d7f7de55756bfe0cf33f1bd30dcbee
Mark Rivers <rivers@cars.uchicago.edu>
2016-04-11 16:25:39 -0500

    When changing queue size empty old queue first and release each NDArray in the queue; fixes memory leak in NDArrayPool

--- ADApp/pluginSrc/NDPluginDriver.cpp
+++ ADApp/pluginSrc/NDPluginDriver.cpp
@@ -166,6 +166,21 @@ void NDPluginDriver::processTask(void)
     this->lock();
     this->pThreadStartedEvent->signal();
     while (1) {
+        /* If the queue size has been changed in the writeInt32 method then create a new one */
+        if (newQueueSize_ > 0) {
+            /* Need to empty the queue and decrease reference count on arrays that were in the queue */
+            while (epicsMessageQueueTryReceive(this->msgQId, &pArray, sizeof(&pArray)) != -1) {
+                pArray->release();
+            }
+            epicsMessageQueueDestroy(this->msgQId);
+            this->msgQId = epicsMessageQueueCreate(newQueueSize_, sizeof(NDArray*));
+            if (!this->msgQId) {
+                asynPrint(pasynUserSelf, ASYN_TRACE_ERROR, 
+                          "%s::%s epicsMessageQueueCreate failure\n", driverName, functionName);
+            }
+            newQueueSize_ = 0;
+        }
+
         /* Wait for an array to arrive from the queue. Release the lock while  waiting. */    
         this->unlock();
         epicsMessageQueueReceive(this->msgQId, &pArray, sizeof(&pArray));
@@ -176,16 +191,6 @@ void NDPluginDriver::processTask(void)
         /* Take the lock.  The function we are calling must release the lock
          * during time-consuming operations when it does not need it. */
         this->lock();
-        /* If the queue size has been changed in the writeInt32 method then create a new one */
-        if (newQueueSize_ > 0) {
-            if (this->msgQId) epicsMessageQueueDestroy(this->msgQId);
-            this->msgQId = epicsMessageQueueCreate(newQueueSize_, sizeof(NDArray*));
-            if (!this->msgQId) {
-                asynPrint(pasynUserSelf, ASYN_TRACE_ERROR, 
-                          "%s::%s epicsMessageQueueCreate failure\n", driverName, functionName);
-            }
-            newQueueSize_ = 0;
-        }
         getIntegerParam(NDPluginDriverQueueSize, &queueSize);
         queueFree = queueSize - epicsMessageQueuePending(this->msgQId);
         setIntegerParam(NDPluginDriverQueueFree, queueFree);
