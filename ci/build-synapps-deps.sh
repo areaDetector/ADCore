9392054cacdcbc2e06105d7c58a51106c005de73
Ulrik Kofoed Pedersen <ulrik.pedersen@diamond.ac.uk>
2016-09-30 13:04:02 +0100

    travis: clone and build ADSupport as external dep.

--- ci/build-synapps-deps.sh
+++ ci/build-synapps-deps.sh
@@ -11,5 +11,12 @@ echo "EPICS_BASE=/usr/lib/epics" > asyn-R4-26/configure/RELEASE
 #echo "EPICS_LIBCOM_ONLY=YES" >> asyn-R4-26/configure/CONFIG_SITE
 make -C asyn-R4-26/
 
+git clone https://github.com/areaDetector/ADSupport.git
+echo "EPICS_BASE=/usr/lib/epics" > ADSupport/configure/RELEASE.linux-x86_64.Common
+#echo "HDF5=/usr"                             >> ADSupport/configure/CONFIG_SITE.linux-x86_64.Common
+#echo "HDF5_LIB=/usr/lib"                     >> ADSupport/configure/CONFIG_SITE.linux-x86_64.Common
+#echo "HDF5_INCLUDE=-I/usr/include"           >> ADSupport/configure/CONFIG_SITE.linux-x86_64.Common
+make -C ADSupport
+
 cd ..
 
--- ci/configure.sh
+++ ci/configure.sh
@@ -7,17 +7,18 @@ set -e
 # with the details of where to find various external libraries.
 echo "EPICS_BASE=/usr/lib/epics"             >> configure/RELEASE.local
 echo "HDF5=/usr"                             >> configure/CONFIG_SITE.linux-x86_64.Common
-echo "HDF5_LIB=/usr/lib"                     >> configure/CONFIG_SITE.linux-x86_64.Common
-echo "HDF5_INCLUDE=-I/usr/include"           >> configure/CONFIG_SITE.linux-x86_64.Common
+#echo "HDF5_LIB=/usr/lib"                     >> configure/CONFIG_SITE.linux-x86_64.Common
+#echo "HDF5_INCLUDE=-I/usr/include"           >> configure/CONFIG_SITE.linux-x86_64.Common
 echo "XML2_INCLUDE=-I/usr/include/libxml2"   >> configure/CONFIG_SITE.linux-x86_64.Common
 echo "BOOST=/usr"                            >> configure/CONFIG_SITE.linux-x86_64.Common
-echo "BOOST_LIB=/usr/lib"                    >> configure/CONFIG_SITE.linux-x86_64.Common
-echo "BOOST_INCLUDE=-I/usr/include"          >> configure/CONFIG_SITE.linux-x86_64.Common
+#echo "BOOST_LIB=/usr/lib"                    >> configure/CONFIG_SITE.linux-x86_64.Common
+#echo "BOOST_INCLUDE=-I/usr/include"          >> configure/CONFIG_SITE.linux-x86_64.Common
 echo "HOST_OPT=NO"                           >> configure/CONFIG_SITE.linux-x86_64.Common 
 echo "USR_CXXFLAGS_Linux=--coverage"         >> configure/CONFIG_SITE.linux-x86_64.Common 
 echo "USR_LDFLAGS_Linux=--coverage"          >> configure/CONFIG_SITE.linux-x86_64.Common 
 
 echo "ASYN=`pwd`/external/asyn-R4-26"        >> configure/RELEASE.local
+echo "ADSUPPORT=`pwd`/external/ADSupport"    >> configure/RELEASE.local
 
 echo "======= configure/RELEASE.local ========================================="
 cat configure/RELEASE.local
