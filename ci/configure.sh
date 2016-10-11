f2ff87c00f6809de1d00983376760b49ff65a063
Ulrik Kofoed Pedersen <ulrik.pedersen@diamond.ac.uk>
2016-10-11 12:54:14 +0100

    travis: configure the WITH_* macro
    travis use system install of hdf5 and boost

--- ci/configure.sh
+++ ci/configure.sh
@@ -6,13 +6,17 @@ set -e
 # Generate the configure/RELEASE.local and configure/CONFIG_SITE.linux-x86_64.Common
 # with the details of where to find various external libraries.
 echo "EPICS_BASE=/usr/lib/epics"             >> configure/RELEASE.local
-echo "HDF5=/usr"                             >> configure/CONFIG_SITE.linux-x86_64.Common
-#echo "HDF5_LIB=/usr/lib"                     >> configure/CONFIG_SITE.linux-x86_64.Common
-#echo "HDF5_INCLUDE=-I/usr/include"           >> configure/CONFIG_SITE.linux-x86_64.Common
+
+echo "WITH_HDF5 = YES"                       >> configure/CONFIG_SITE.linux-x86_64.Common
+echo "HDF5_EXTERNAL = YES"                   >> configure/CONFIG_SITE.linux-x86_64.Common
+
+echo "WITH_XML2     = YES"                   >> configure/CONFIG_SITE.linux-x86_64.Common
+echo "XML2_EXTERNAL = YES"                   >> configure/CONFIG_SITE.linux-x86_64.Common
 echo "XML2_INCLUDE=-I/usr/include/libxml2"   >> configure/CONFIG_SITE.linux-x86_64.Common
-echo "BOOST=/usr"                            >> configure/CONFIG_SITE.linux-x86_64.Common
-#echo "BOOST_LIB=/usr/lib"                    >> configure/CONFIG_SITE.linux-x86_64.Common
-#echo "BOOST_INCLUDE=-I/usr/include"          >> configure/CONFIG_SITE.linux-x86_64.Common
+
+echo "WITH_BOOST     = YES"                  >> configure/CONFIG_SITE.linux-x86_64.Common
+echo "BOOST_EXTERNAL = YES"                  >> configure/CONFIG_SITE.linux-x86_64.Common
+
 echo "HOST_OPT=NO"                           >> configure/CONFIG_SITE.linux-x86_64.Common 
 echo "USR_CXXFLAGS_Linux=--coverage"         >> configure/CONFIG_SITE.linux-x86_64.Common 
 echo "USR_LDFLAGS_Linux=--coverage"          >> configure/CONFIG_SITE.linux-x86_64.Common 
