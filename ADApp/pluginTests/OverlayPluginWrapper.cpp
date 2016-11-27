c7e19a6097bd95ca9bb93ea4cf6601d1ff83c147
Mark Rivers <rivers@cars.uchicago.edu>
2016-11-26 19:17:52 -0600

    Removed addr argument, not yet supported

--- ADApp/pluginTests/OverlayPluginWrapper.cpp
+++ ADApp/pluginTests/OverlayPluginWrapper.cpp
@@ -1,20 +1,19 @@
 /*
  * OverlayPluginWrapper.cpp
  *
- *  Created on: 9 Nov 2016
+ *  Created on: 26 Nov 2016
  *      Author: Mark Rivers
  */
 
 #include "OverlayPluginWrapper.h"
 
 OverlayPluginWrapper::OverlayPluginWrapper(const std::string& port, const std::string& detectorPort, int maxOverlays)
-  :  NDPluginOverlay(port.c_str(), int addr, 50, 0, detectorPort.c_str(), 0, maxOverlays, 0, 0, 0, 0),
-     AsynPortClientContainer(port, addr)
+  :  NDPluginOverlay(port.c_str(), 50, 0, detectorPort.c_str(), 0, maxOverlays, 0, 0, 0, 0),
+     AsynPortClientContainer(port)
 {
 }
 
 OverlayPluginWrapper::OverlayPluginWrapper(const std::string& port,
-                                   int addr,
                                    int queueSize,
                                    int blocking,
                                    const std::string& detectorPort,
@@ -26,7 +25,7 @@ OverlayPluginWrapper::OverlayPluginWrapper(const std::string& port,
   :  NDPluginOverlay(port.c_str(), queueSize, blocking,
                         detectorPort.c_str(), address, maxOverlays,
                         0, maxMemory, priority, stackSize),
-     AsynPortClientContainer(port, addr)
+     AsynPortClientContainer(port)
 {
 }
 
