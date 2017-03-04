34bac1cbdd2ab3fada42469596ec8bb09d83711b
Mark Rivers <rivers@cars.uchicago.edu>
2017-03-04 15:03:18 -0600

    Implemented sorting

--- ADApp/pluginSrc/NDPluginGather.h
+++ ADApp/pluginSrc/NDPluginGather.h
@@ -1,10 +1,19 @@
 #ifndef NDPluginGather_H
 #define NDPluginGather_H
 
+#include <set>
+
 #include "NDPluginDriver.h"
 
+typedef enum {
+    NDGatherCallbacksUnsorted,
+    NDGatherCallbacksSorted
+} NDGatherCallbacksSorted_t;
+
 /* General parameters */
-#define NDPluginGatherDummyString          "GATHER_DUMMY"            /* (asynInt32,        r/w) dummy parameter */
+#define NDPluginGatherSortModeString  "GATHER_SORT_MODE"            /* (asynInt32,        r/w) sorted callback mode */
+#define NDPluginGatherSortTimeString  "GATHER_SORT_TIME"            /* (asynFloat64,      r/w) sorted callback time */
+#define NDPluginGatherListFreeString  "GATHER_LIST_FREE"            /* (asynInt32,        r/o) std::multilist free elements */
 
 typedef struct {
     void *asynGenericPointerInterruptPvt;        /**< InterruptPvt for connecting to NDArray driver interupts */
@@ -22,17 +31,23 @@ public:
                    int maxBuffers, size_t maxMemory,
                    int priority, int stackSize);
 
+    void sortingTask();
+
 protected:
     /* These methods override the virtual methods in the base class */
     virtual void processCallbacks(NDArray *pArray);
     virtual asynStatus connectToArrayPort(void);    
     virtual asynStatus setArrayInterrupt(int connect);
-    int NDPluginGatherDummy;
-    #define FIRST_NDPLUGIN_GATHER_PARAM NDPluginGatherDummy
+
+    int NDPluginGatherSortMode;
+    #define FIRST_NDPLUGIN_GATHER_PARAM NDPluginGatherSortMode
+    int NDPluginGatherSortTime;
+    int NDPluginGatherListFree;
                                 
 private:
     int maxPorts_;
     NDGatherNDArraySource_t *NDArraySrc_;
+    std::multiset<class sortedListElement> sortedNDArrayList_;
 };
     
 #endif
