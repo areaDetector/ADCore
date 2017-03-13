18055d1da6cc4f24e9eb1075983990336c98c197
Mark Rivers <rivers@cars.uchicago.edu>
2017-03-13 17:59:14 -0500

    Minor tweak

--- ADApp/pluginSrc/test_forward_reference.cpp
+++ ADApp/pluginSrc/test_forward_reference.cpp
@@ -2,8 +2,7 @@
 
 void test_forward_reference()
 {
-
-// Creating the object as an automatic variable causes compilation error on Linux but builds fine on Windows
+// Creating the object as an automatic variable causes a compilation error on Linux but compiles fine on Windows
   NDPluginStats stats = NDPluginStats("STATS1", 20, 0, "SIM1", 0, 0, 0, 0, 0);
 
 // Creating the object as a heap variable works fine on Linux and on Windows
