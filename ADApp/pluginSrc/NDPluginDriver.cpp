67b6dce769de4597baaa0f12c8e8abdb1428e208
Mark Rivers <rivers@cars.uchicago.edu>
2017-03-24 15:10:46 -0500

    Fix bug; this->pArrays[0] was not being set if ArrayCallbacks was false

--- ADApp/pluginSrc/NDPluginDriver.cpp
+++ ADApp/pluginSrc/NDPluginDriver.cpp
@@ -240,7 +240,14 @@ asynStatus NDPluginDriver::endProcessCallbacks(NDArray *pArray, bool copyArray, 
     static const char *functionName = "endProcessCallbacks";
 
     getIntegerParam(NDArrayCallbacks, &arrayCallbacks);
-    if (arrayCallbacks != 1) return asynSuccess;
+    if (arrayCallbacks == 0) {
+        // We don't do array callbacks but still want to cache the last array in pArrays[0]
+        // If this array has not been copied then we need to increase the reference count
+        if (!copyArray) pArray->reserve();
+        if (this->pArrays[0]) this->pArrays[0]->release();
+        this->pArrays[0] = pArray;
+        return asynSuccess;
+    }
 
     getIntegerParam(NDPluginDriverSortMode, &callbacksSorted);
     if (copyArray) {
