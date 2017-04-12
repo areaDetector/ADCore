a55f540d11bebd236387fe60626b49f78a49b4a4
Mark Rivers <rivers@cars.uchicago.edu>
2017-04-12 12:18:42 -0500

    Removed WITH_XML2 because xml2 is now required

--- ci/configure.sh
+++ ci/configure.sh
@@ -10,7 +10,6 @@ echo "EPICS_BASE=/usr/lib/epics"             >> configure/RELEASE.local
 echo "WITH_HDF5 = YES"                       >> configure/CONFIG_SITE.linux-x86_64.Common
 echo "HDF5_EXTERNAL = YES"                   >> configure/CONFIG_SITE.linux-x86_64.Common
 
-echo "WITH_XML2     = YES"                   >> configure/CONFIG_SITE.linux-x86_64.Common
 echo "XML2_EXTERNAL = YES"                   >> configure/CONFIG_SITE.linux-x86_64.Common
 echo "XML2_INCLUDE=-I/usr/include/libxml2"   >> configure/CONFIG_SITE.linux-x86_64.Common
 
