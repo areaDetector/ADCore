d5b4b800864b8f18ed3eb610a1e24e6f5e058f27
Mark Rivers <rivers@cars.uchicago.edu>
2018-06-21 12:07:35 -0500

    Reverted from std::vector to new/delete for dims_t; more efficient and cleaner IMHO

--- ADApp/ADSrc/NDArray.cpp
+++ ADApp/ADSrc/NDArray.cpp
@@ -126,10 +126,10 @@ int NDArray::getInfo(NDArrayInfo_t *pInfo)
 {
   int i;
   NDAttribute *pAttribute;
-  std::vector<size_t> dims_t;
-  dims_t.reserve(ndims);
-  for (i=0; i<this->ndims; i++) dims_t.push_back(this->dims[i].size);
-  int status = NDArray::computeArrayInfo(this->ndims, &dims_t[0], this->dataType, pInfo);
+  size_t *dims_t = new size_t(this->ndims);
+  for (i=0; i<this->ndims; i++) dims_t[i] = this->dims[i].size;
+  int status = NDArray::computeArrayInfo(this->ndims, dims_t, this->dataType, pInfo);
+  delete dims_t;
   if (status != ND_SUCCESS) return status;
   
   pInfo->colorMode = NDColorModeMono;
