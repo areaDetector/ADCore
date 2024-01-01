563890f919cf08c8ce49630d2c425e036e795e46
Mark Rivers <rivers@cars.uchicago.edu>
2024-01-01 15:21:22 -0600

    Updated version to 3.13.0

--- ADApp/ADSrc/ADCoreVersion.h
+++ ADApp/ADSrc/ADCoreVersion.h
@@ -2,8 +2,8 @@
 #define ADCoreVersion_H
 
 #define ADCORE_VERSION      3
-#define ADCORE_REVISION     12
-#define ADCORE_MODIFICATION 1
+#define ADCORE_REVISION     13
+#define ADCORE_MODIFICATION 0
 
 #endif
 
